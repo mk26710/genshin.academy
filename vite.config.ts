38374301ed31a3597f1ba1941be20c555cf06405
Kitsune Guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-04-25 19:23:26 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({ 
+export default defineConfig({
+  // please just start working im literally doing this according to the official guides
   base: "/trying-vue/",
   plugins: [vue()]
 })
