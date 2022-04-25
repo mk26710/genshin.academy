be56dfc2434902ecdd76c4fbd43b48c7bcc992bc
Kitsune Guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-04-25 19:21:21 +0400

    this github pages thing is driving me crazy ngl

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
   base: "/trying-vue/",
   plugins: [vue()]
 })
