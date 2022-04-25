310d6444ac2c9fd3279bc3950ae758f877849323
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-04-25 18:18:24 +0400

    alright might give a go to gh pages too

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // uncomment before deploying to gh pages
+  // base: "/trying-vue/",
   plugins: [vue()]
 })
