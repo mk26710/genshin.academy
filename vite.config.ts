77c223837541b729860a0aeb3a1d2f8a0b12fecc
Kitsune Guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-04-25 19:30:59 +0400

    maybe this time it'll work as I expect?

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // please just start working im literally doing this according to the official guides
   base: "/trying-vue/",
   plugins: [vue()]
 })
