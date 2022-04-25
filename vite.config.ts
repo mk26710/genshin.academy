59f43aaf09e8fcf94a7add68cac7fdabe9d423ea
Kitsune Guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-04-25 18:45:49 +0400

    not needed anymore because actions are used

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // uncomment before deploying to gh pages
-  // base: "/trying-vue/",
   plugins: [vue()]
 })
