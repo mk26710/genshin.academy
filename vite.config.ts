8e12e0ee769c060d58bc767d248a79a0c26e38ee
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-04-26 18:26:13 +0400

    ignore annoying warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   base: "/trying-vue/",
   plugins: [vue()]
