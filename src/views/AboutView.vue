05b6e9d849ccb4ac521403a19f1cf5a02ee43036
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-04-26 18:28:14 +0400

    use pinia store instead of ref values

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import {ref} from 'vue';
 import ResinCalculated from '../components/ResinCalculated.vue';
+import {useCalcStoreRefs} from "../stores/CalculatorStore"
 
-const current = ref(0);
-const target = ref(160);
+const {resin} = useCalcStoreRefs()
 </script>
 
 <template>
   <h1>yoo we're very cool company did you know that?</h1>
   <p>summertime is very soon finally</p>
   <p>you can calculate resin here btw</p>
-  <input v-model="current" placeholder="your current resin"/>
-  <input v-model="target" placeholder="you need resin"/>
+  <input v-model="resin.current" placeholder="your current resin"/>
+  <input v-model="resin.needed" placeholder="you need resin"/>
   <div>
-    <ResinCalculated :current="current" :target="target"/>
+    <ResinCalculated/>
   </div>
   <p>here's another commit and now that will trigger the deployment</p>
 </template>
