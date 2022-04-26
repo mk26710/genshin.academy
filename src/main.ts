541f8e5b7f3b0d77483ef408e6b76b15d1f14aad
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-04-26 18:26:55 +0400

    use pinia in main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { store, key as InjectionKey } from "./store";
+import {createPinia} from 'pinia'
 
 createApp(App)
-    .use(store, InjectionKey)
+    .use(createPinia())
     .use(router)
     .mount("#app");
