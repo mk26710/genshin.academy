756a1e541087af821e1ce948eb8538a1fdc69c26
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-01 17:06:22 +0400

    used some tailwind classes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,13 +6,15 @@ const { resin } = useCalcStoreRefs();
 </script>
 
 <template>
-  <h1>yoo we're very cool company did you know that?</h1>
-  <p>summertime is very soon finally</p>
-  <p>you can calculate resin here btw</p>
-  <input v-model="resin.current" placeholder="your current resin" />
-  <input v-model="resin.needed" placeholder="you need resin" />
-  <div>
-    <ResinCalculated />
+  <div class="container mx-auto">
+    <h1>yoo we're very cool company did you know that?</h1>
+    <p>summertime is very soon finally</p>
+    <p>you can calculate resin here btw</p>
+    <input v-model="resin.current" placeholder="your current resin" />
+    <input v-model="resin.needed" placeholder="you need resin" />
+    <div>
+      <ResinCalculated />
+    </div>
+    <p>here's another commit and now that will trigger the deployment</p>
   </div>
-  <p>here's another commit and now that will trigger the deployment</p>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>hello this is home</h1>
-  <p>fun fact: i don't touch grass</p>
-  <p>trying out github actions, so that line is one commit</p>
+  <div class="container mx-auto">
+    <h1>hello this is home</h1>
+    <p>fun fact: i don't touch grass</p>
+    <p>trying out github actions, so that line is one commit</p>
+  </div>
 </template>
