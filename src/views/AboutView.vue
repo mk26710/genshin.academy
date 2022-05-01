cbcf9339523acccf9eb63163e71dcff8e1499ff2
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-01 19:44:55 +0400

    [Deploy] Removed unnecessary text and improved styling

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,21 +9,22 @@ const { resin } = useCalcStoreRefs();
   <div class="flex flex-col grow mx-auto container justify-center items-center">
     <div>
       <h1 class="dark:text-white font-bold text-4xl">Useless resin calculator</h1>
-      <p>summertime is very soon finally</p>
       <p>you can calculate resin here btw</p>
       <div class="flex flex-row gap-2 my-2">
         <input
           class="text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700"
-          v-model="resin.current" placeholder="your current resin" />
+          v-model="resin.current"
+          type="number"
+          placeholder="your current resin" />
         <input
           class="text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700"
-          v-model="resin.needed" placeholder="you need resin" />
+          v-model="resin.needed"
+          type="number"
+          placeholder="you need resin" />
       </div>
-      <div>
+      <div class="font-bold text-pink-500 dark:text-pink-300">
         <ResinCalculated />
       </div>
-      <p>here's another commit and now that will trigger the deployment</p>
     </div>
-
   </div>
 </template>
