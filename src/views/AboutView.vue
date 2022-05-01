be98e02413ae0c68fc58f957505cc26b8efb186d
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-01 19:23:58 +0400

    [Deploy] cool input field which is definitely not from tailwind docs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,15 +6,24 @@ const { resin } = useCalcStoreRefs();
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <h1>yoo we're very cool company did you know that?</h1>
-    <p>summertime is very soon finally</p>
-    <p>you can calculate resin here btw</p>
-    <input v-model="resin.current" placeholder="your current resin" />
-    <input v-model="resin.needed" placeholder="you need resin" />
+  <div class="flex flex-col grow mx-auto container justify-center items-center">
     <div>
-      <ResinCalculated />
+      <h1 class="text-white font-bold text-4xl">Useless resin calculator</h1>
+      <p>summertime is very soon finally</p>
+      <p>you can calculate resin here btw</p>
+      <div class="flex flex-row gap-2 my-2">
+        <input
+          class="text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700"
+          v-model="resin.current" placeholder="your current resin" />
+        <input
+          class="text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700"
+          v-model="resin.needed" placeholder="you need resin" />
+      </div>
+      <div>
+        <ResinCalculated />
+      </div>
+      <p>here's another commit and now that will trigger the deployment</p>
     </div>
-    <p>here's another commit and now that will trigger the deployment</p>
+
   </div>
 </template>
