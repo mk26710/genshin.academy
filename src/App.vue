c9373f6be79de028d76ea9f232c29f196e87c04f
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-01 19:28:15 +0400

    [Deploy] Fixed missing lang attributes in some places

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import Nav from "./components/Navigation/Navbar.vue";
 import Footer from "./components/Navigation/Footer.vue";
 </script>
--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { CodeIcon } from "@heroicons/vue/solid";
 </script>
 
