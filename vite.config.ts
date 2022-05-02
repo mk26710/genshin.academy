332102500960eb1eab50fbeec02f61bc1c635bca
Kitsune Guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-02 21:58:42 +0400

    [Deploy] Fix assetsInclude and missing comma

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   base: "/trying-vue/",
-  assetsInclude: ["./src/data/*.json"]
+  assetsInclude: ["./src/data/**/*.json"],
   plugins: [vue()],
   resolve: {
     alias: {
