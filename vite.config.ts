c1b7393547c13f492d79db20df575cf436d417dc
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-04 15:28:57 +0400

    idk vscode keeps screaming at me for this :/

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import * as path from "path";
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
