261ae1df22e0736aa5cbcbcd5fe1806a12ba19d7
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-07 22:46:56 +0400

    404 page uses main container, reduced the gap

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,10 @@
 <template>
-  <main class="flex grow justify-center text-center items-center">
-    <div class="container mx-auto max-w-xl">
+  <main-container :vertical-center="true">
+    <div class="container mx-auto max-w-xl text-center">
       <h1 class="dark:text-white font-bold text-5xl mb-2">404: Not Found</h1>
       <p>You are trying to visit a non existing page :(</p>
 
-      <div class="mt-24">
+      <div class="mt-12">
         <router-link to="/">
           <button
             class="py-2 px-3 transition-colors duration-300 ease-in-out font-bold bg-rose-500 hover:bg-rose-400 text-rose-200 hover:text-sky-100 shadow-lg shadow-rose-500/50 hover:shadow-rose-400/50 rounded"
@@ -14,5 +14,5 @@
         </router-link>
       </div>
     </div>
-  </main>
+  </main-container>
 </template>
