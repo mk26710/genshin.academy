22b3a8f3314e48f9aa00cbc83a3c63cc2ea753e9
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-07 22:36:59 +0400

    changed the way bound classes are generated
    now main-container has a boundClasses() funtion that generates an array of classes that are based of passed or not passed props. that way it's a little less messy and easier to read from my perspective.

--- src/components/base/MainContainer.vue
+++ src/components/base/MainContainer.vue
@@ -4,18 +4,21 @@
     verticalCenter?: boolean;
   }
 
-  withDefaults(defineProps<Props>(), {
+  const props = withDefaults(defineProps<Props>(), {
     grow: true,
     verticalCenter: false,
   });
+
+  const boundClasses = () => {
+    return [
+      ...(props.grow ? ["grow"] : []),
+      ...(props.verticalCenter ? ["flex", "flex-col", "justify-center"] : []),
+    ];
+  };
 </script>
 
-<!-- not really sure if this approach for condtional classes is correct, but it works lol -->
 <template>
-  <main
-    class="container max-w-4xl self-center px-4 sm:px-10 lg:px-0"
-    :class="[grow && ['grow'], verticalCenter && ['flex', 'flex-col', 'justify-center']]"
-  >
+  <main class="container max-w-4xl self-center px-4 sm:px-10 lg:px-0" :class="boundClasses()">
     <slot></slot>
   </main>
 </template>
