18020839796eba2ec118773dd3785371e2f7c1e7
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-07 22:03:05 +0400

    created main container component

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  interface Props {
+    grow?: boolean;
+    verticalCenter?: boolean;
+  }
+
+  withDefaults(defineProps<Props>(), {
+    grow: true,
+    verticalCenter: false,
+  });
+</script>
+
+<!-- not really sure if this approach for condtional classes is correct, but it works lol -->
+<template>
+  <main
+    class="container max-w-4xl self-center px-4 sm:px-10 lg:px-0"
+    :class="[grow && ['grow'], verticalCenter && ['flex', 'flex-col', 'justify-center']]"
+  >
+    <slot></slot>
+  </main>
+</template>
