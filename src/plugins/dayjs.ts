8ee481eb42fa649a7b0c2251b55458089f51f972
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-08 16:18:38 +0400

    provide dayjs at app level

--- src/plugins/dayjs.ts
+++ src/plugins/dayjs.ts
@@ -1,8 +1,11 @@
-import { App } from "vue";
+import { App, InjectionKey } from "vue";
 import dayjs from "dayjs";
+
+export const dayjsKey: InjectionKey<typeof dayjs> = Symbol("dayjs");
 
 export default {
   install: (app: App) => {
     app.config.globalProperties.$dayjs = dayjs;
+    app.provide(dayjsKey, dayjs);
   },
 };
