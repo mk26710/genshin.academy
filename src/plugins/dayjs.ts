e808501e36f3ffa916bdd503454a42c917a791a3
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-08 16:51:06 +0400

    yep, readonly was a mistkae

--- src/plugins/dayjs.ts
+++ src/plugins/dayjs.ts
@@ -1,12 +1,11 @@
-import { App, InjectionKey, readonly } from "vue";
 import dayjs from "dayjs";
+import { App, InjectionKey } from "vue";
 
 export const dayjsKey: InjectionKey<typeof dayjs> = Symbol("dayjs");
 
 export default {
   install: (app: App) => {
     app.config.globalProperties.$dayjs = dayjs;
-    // readonly here could cause some unexpected behaviour, so gotta keep that in mind
-    app.provide(dayjsKey, readonly(dayjs));
+    app.provide(dayjsKey, dayjs);
   },
 };
