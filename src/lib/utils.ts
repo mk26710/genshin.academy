3cdb7c44e73ebd5e11a5c97e05edac0bd821f468
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-08 16:17:55 +0400

    utility function for type safe injection

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+// very useful article on type safe injection - https://logaretm.com/blog/type-safe-provide-inject/
+import { inject, InjectionKey } from "vue";
+
+export const injectStrict = <T>(key: InjectionKey<T>, fallback?: T) => {
+  const resolved = inject(key, fallback);
+  if (!resolved) {
+    throw new Error(`${key.description} was not resolved.`);
+  }
+
+  return resolved;
+};
