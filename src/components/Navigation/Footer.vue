0c6eb36effc12c6ad8635ebfc200f0773e083407
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-09 20:56:17 +0400

    added some padding to footer

--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <footer class="self-center lg:ml-48">
+  <footer class="self-center lg:ml-48 py-4">
     <span class="transition-colors duration-300 ease-in-out hover:text-pink-300">
       <a href="https://github.com/kitsune-guuji/trying-vue" target="_blank">
         <CodeIcon class="h-5 w-5 inline" />
