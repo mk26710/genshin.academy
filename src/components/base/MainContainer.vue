617e782415b288e34acbb9e6cb5ad2e9cf996e36
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-10 03:20:46 +0400

    class binding function can be inlined

--- src/components/base/MainContainer.vue
+++ src/components/base/MainContainer.vue
@@ -6,14 +6,13 @@
   const props = withDefaults(defineProps<Props>(), {
     verticalCenter: false,
   });
-
-  const boundClasses = () => {
-    return [...(props.verticalCenter ? ["place-content-center"] : [])];
-  };
 </script>
 
 <template>
-  <main class="main px-4 pt-0 lg:pt-4 grid" :class="boundClasses()">
+  <main
+    class="main px-4 pt-0 lg:pt-4 grid"
+    :class="props.verticalCenter ? 'place-content-center' : ''"
+  >
     <div id="content">
       <slot></slot>
     </div>
