924e0e8ab8a8ba7544a5b060e14d53a110738bbc
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-09 20:49:27 +0400

    rename navbar root to nav

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-  <header class="block lg:hidden self-center my-2">
+  <nav class="block lg:hidden self-center my-2">
     <router-link to="/">Home </router-link>
     |
     <router-link to="/about">About </router-link>
     |
     <router-link to="/guides">Guides</router-link>
-  </header>
+  </nav>
 </template>
