5cac215d55af6d39e806339aafccd8bb87a05184
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-09 20:50:13 +0400

    reored links on navbar

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -2,8 +2,8 @@
   <nav class="block lg:hidden self-center my-2">
     <router-link to="/">Home </router-link>
     |
-    <router-link to="/about">About </router-link>
-    |
     <router-link to="/guides">Guides</router-link>
+    |
+    <router-link to="/about">About </router-link>
   </nav>
 </template>
