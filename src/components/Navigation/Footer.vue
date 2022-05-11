1cac2d8e7764212e3381949b227d847bb4dd8ac5
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-11 23:33:34 +0400

    source code button has lower opacity now

--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -7,7 +7,7 @@ const { t } = useI18n();
 
 <template>
   <footer class="footer grid place-content-center py-4">
-    <span class="transition-colors duration-300 ease-in-out hover:text-pink-300">
+    <span class="transition-all duration-300 ease-in-out opacity-30 hover:text-pink-300 hover:opacity-100">
       <a href="https://github.com/kitsune-guuji/trying-vue" target="_blank">
         <CodeIcon class="h-5 w-5 inline" />
         <span class="font-bold">&nbsp;{{ t("generic.sourceCode") }}</span>
