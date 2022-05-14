1c0eaae35b966915bc6350a5ebee63226a5649a5
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-14 20:28:41 +0400

    cards can now be a div or a link

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
+import DivOrLink from "./DivOrLink.vue"
+
 interface Props {
   title: string;
   description?: string;
   publishedAt?: Date;
   thumbnail?: string;
+  to?: string;
 }
 
 withDefaults(defineProps<Props>(), {});
 </script>
 
 <template>
-  <div class="flex flex-col border rounded-xl">
+  <div-or-link :to="to" class="flex flex-col border rounded-xl">
     <div>
       <img v-if="!!thumbnail" :src="thumbnail" class="object-fill object-top" />
     </div>
@@ -18,12 +21,12 @@ withDefaults(defineProps<Props>(), {});
       <div v-if="!!publishedAt" class="font-medium text-sm leading-6 text-pink-500">
         {{ $dayjs(publishedAt).format("HH:mm on DD.MM.YYYY") }}
       </div>
-      <a href="#" class="block font-semibold text-base text-slate-900 leading-6 hover:underline">
+      <p class="block font-semibold text-base text-slate-900 leading-6">
         {{ title }}
-      </a>
+      </p>
       <p v-if="!!description" class="text-sm text-justify text-slate-600 leading-6">
         {{ description }}
       </p>
     </div>
-  </div>
+  </div-or-link>
 </template>
