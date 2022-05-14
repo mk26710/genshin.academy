96ed9545cc75ed83715e06de02bc2149c0cfe075
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-14 20:28:03 +0400

    created a wrapper component for cards

---
+++ src/components/DivOrLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  to?: string;
+}
+
+withDefaults(defineProps<Props>(), {});
+</script>
+
+<template>
+  <router-link v-if="!!to" :to="to">
+    <slot />
+  </router-link>
+
+  <div v-else>
+    <slot />
+  </div>
+</template>
