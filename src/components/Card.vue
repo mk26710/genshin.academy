683dc78a9a7e61356c9f9f19209330a9d373761e
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-14 19:26:18 +0400

    card component

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  description?: string;
+  publishedAt?: Date;
+  thumbnail?: string;
+}
+
+withDefaults(defineProps<Props>(), {});
+</script>
+
+<template>
+  <div class="flex flex-col border rounded-xl">
+    <div>
+      <img v-if="!!thumbnail" :src="thumbnail" class="object-fill object-top" />
+    </div>
+    <div class="p-6 space-y-2">
+      <div v-if="!!publishedAt" class="font-medium text-sm leading-6 text-pink-500">
+        {{ $dayjs(publishedAt).format("HH:mm on DD.MM.YYYY") }}
+      </div>
+      <a href="#" class="block font-semibold text-base text-slate-900 leading-6 hover:underline">
+        {{ title }}
+      </a>
+      <p v-if="!!description" class="text-sm text-justify text-slate-600 leading-6">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
