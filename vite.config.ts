b740aceb8001f7286e6176adfe864baf8c350ae2
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-17 18:11:57 +0400

    specified path for vueI18n files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
 import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 import { URL, fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-  plugins: [vue(), eslintPlugin(), vueI18n()],
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    vueI18n({ include: path.resolve(__dirname, "./src/lang/**") }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
