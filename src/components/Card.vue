75dae51d1d289516b5ebd840758c39638b24878d
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-17 18:27:27 +0400

    some minor styling changes to guide cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import DivOrLink from "./DivOrLink.vue"
+import DivOrLink from "./DivOrLink.vue";
 
 interface Props {
   title: string;
@@ -13,11 +13,11 @@ withDefaults(defineProps<Props>(), {});
 </script>
 
 <template>
-  <div-or-link :to="to" class="flex flex-col border rounded-xl">
+  <div-or-link :to="to" class="flex flex-col border rounded-md">
     <div class="min-h-[10rem] lg:min-h-[20rem] bg-neutral-200">
       <img v-if="!!thumbnail" :src="thumbnail" loading="lazy" class="object-fill object-top" />
     </div>
-    <div class="p-6 space-y-2">
+    <div class="px-6 py-4 space-y-2">
       <div v-if="!!publishedAt" class="font-medium text-sm leading-6 text-pink-500">
         {{ $dayjs(publishedAt).format("HH:mm on DD.MM.YYYY") }}
       </div>
