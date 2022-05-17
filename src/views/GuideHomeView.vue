ebd6e16e11b212e14184d19af97657d880b37a5d
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-17 18:24:28 +0400

    guides page is now more dynamic and has search

--- src/views/GuideHomeView.vue
+++ src/views/GuideHomeView.vue
@@ -1,26 +1,40 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 import Card from "@/components/Card.vue";
 
-const ayakaAvatarHeader = new URL(
-  "../assets/characters/kamisato_ayaka/avatar_header.png",
-  import.meta.url,
-);
+import { data as publishedCharacters } from "@/data/guides/published.json";
+
+const query = ref("");
+
+const characterAvatar = (id: string) =>
+  new URL(`../assets/characters/${id}/avatar_header.png`, import.meta.url).href;
+
+const search = (name: string) =>
+  publishedCharacters
+    .sort((a, b) => b.publishedAt - a.publishedAt)
+    .filter((c) => c.title.toLowerCase().includes(name.toLowerCase()));
 </script>
 
 <template>
   <main-container>
-    <h1 class="font-bold text-4xl text-center mb-4">some good guides</h1>
+    <input
+      placeholder="Search by title"
+      v-model="query"
+      class="w-full mb-4 lg:mb-8 leading-6 accent-pink-300 rounded-md ring-1 ring-neutral-900/10 shadow-sm py-1.5 pl-2 pr-3"
+    />
+
     <div
       class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2"
     >
       <Card
-        v-for="i in 50"
-        :key="i"
-        to="/guides/kamisato_ayaka"
-        title="Kamisato Ayaka"
-        :thumbnail="ayakaAvatarHeader.href"
-        :published-at="new Date()"
-        description="The true archon of Eternity. The embodiment of an absolute beauty and elegance. She is all you need for your entire adventure."
+        v-for="character in search(query)"
+        :key="character.id"
+        :to="`/guides/${character.id}`"
+        :title="character.title"
+        :thumbnail="characterAvatar(character.id)"
+        :published-at="new Date(character.publishedAt * 1000)"
+        :description="character.brief"
       />
     </div>
   </main-container>
