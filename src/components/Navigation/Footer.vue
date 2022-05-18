149ca2caaed3191156c52fbd879478d7ef936d2b
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-18 15:02:17 +0400

    suppressed some eslint complaints

--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -1,10 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import { CodeIcon } from "@heroicons/vue/solid";
 </script>
 
 <template>
   <footer class="footer grid place-content-center py-4">
-    <span class="transition-all duration-300 ease-in-out opacity-30 hover:text-pink-300 hover:opacity-100">
+    <span
+      class="transition-all duration-300 ease-in-out opacity-30 hover:text-pink-300 hover:opacity-100"
+    >
       <a href="https://github.com/kitsune-guuji/trying-vue" target="_blank">
         <CodeIcon class="h-5 w-5 inline" />
         <span class="font-bold">&nbsp;source code</span>
--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import { MenuIcon } from "@heroicons/vue/solid";
 import { computed, ref } from "vue";
