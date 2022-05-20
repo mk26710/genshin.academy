498a4fbeab5289c85fbd4f1ac12ff3235ae24282
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-20 19:35:28 +0400

    fix formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from "url";
-
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { URL, fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
