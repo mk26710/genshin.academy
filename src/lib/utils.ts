71de7f20a0179e938b57ae92fd964609d37adbba
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-22 21:30:06 +0400

    fix complaining eslint

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,11 @@
 // very useful article on type safe injection - https://logaretm.com/blog/type-safe-provide-inject/
-import { InjectionKey, inject } from "vue";
+import { inject } from "vue";
+import type { InjectionKey } from "vue";
 
 export const injectStrict = <T>(key: InjectionKey<T>, fallback?: T) => {
   const resolved = inject(key, fallback);
   if (!resolved) {
-    throw new Error(`${key.description} was not resolved.`);
+    throw new Error(`${key} was not resolved.`);
   }
 
   return resolved;
