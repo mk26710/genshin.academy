c81760c052accf6d5c3aa38237c488b4fe812400
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-22 19:33:46 +0400

    udpate import order

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createPinia } from "pinia";
 import { createApp } from "vue";
 
 import App from "@/App.vue";
+
 import dayjs from "@/plugins/dayjs";
-// import i18n from "@/plugins/i18n";
 import router from "@/router";
 
 import "./assets/styles/main.scss";
@@ -13,6 +13,5 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(dayjs);
-// .use(i18n)
 
 app.mount("#app");
