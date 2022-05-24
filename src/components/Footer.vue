480b60b34e00742b055a97e42aff737d553cd7b4
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-24 23:56:20 +0400

    updated footer contents

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts">
-import { CodeIcon } from "@heroicons/vue/solid";
-</script>
-
 <template>
-  <footer class="footer grid place-content-center py-4">
-    <span
-      class="transition-all duration-300 ease-in-out opacity-30 hover:text-pink-300 hover:opacity-100"
-    >
-      <a href="https://github.com/kitsune-guuji/trying-vue" target="_blank">
-        <CodeIcon class="h-5 w-5 inline" />
-        <span class="font-bold">&nbsp;source code</span>
-      </a>
-    </span>
+  <footer class="footer flex flex-col lg:flex-row py-4 mx-4 border-t border-neutral-200">
+    <p class="opacity-50 flex-grow">
+      genshin.zenless.club is not affiliated with
+      <a class="text-sky-600" target="_blank" href="https://www.mihoyo.com/">miHoYo</a>. <br />
+      All in-game content is the property of
+      <a class="text-sky-600" target="_blank" href="https://www.mihoyo.com/">miHoYo Co., Ltd</a>.
+    </p>
   </footer>
 </template>
