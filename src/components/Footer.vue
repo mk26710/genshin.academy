5046dabb16a8f98fdd6034ef6f5f903aad569631
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-25 21:59:17 +0400

    updated footer once again, should look better

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,30 +6,42 @@ import GitHubLogo from "@/components/icons/GitHubLogo.vue";
 
 <template>
   <footer
-    class="footer grid grid-cols-1 lg:grid-cols-[1fr_1fr] grid-rows-[auto_auto] lg:grid-rows-1 py-4 mx-4 border-t border-neutral-200"
+    class="footer grid grid-cols-1 lg:grid-cols-[1fr_auto] grid-rows-[auto_auto] lg:grid-rows-1 py-4 mx-4 border-t border-neutral-200"
   >
-    <p class="opacity-50">
+    <p class="text-sm text-neutral-400">
       genshin.zenless.club is not affiliated with
-      <a class="text-sky-600" target="_blank" href="https://www.mihoyo.com/">miHoYo</a>. <br />
+      <a
+        class="text-primary-400 hover:text-primary-600 transition-colors ease-in-out duration-200"
+        target="_blank"
+        href="https://www.mihoyo.com/"
+        >miHoYo</a
+      >. <br />
       All in-game content is the property of
-      <a class="text-sky-600" target="_blank" href="https://www.mihoyo.com/">miHoYo Co., Ltd</a>.
+      <a
+        class="text-primary-400 hover:text-primary-600 transition-colors ease-in-out duration-200"
+        target="_blank"
+        href="https://www.mihoyo.com/"
+        >miHoYo Co., Ltd</a
+      >.
     </p>
 
-    <div class="flex flex-col lg:flex-row mt-4 lg:mt-0 gap-x-6 gap-y-2 font-bold text-lg">
+    <div
+      class="place-self-start lg:place-self-center flex flex-col lg:flex-row gap-x-6 gap-y-2 mt-6 lg:mt-0 font-bold text-sm text-neutral-400"
+    >
       <a
         href="https://github.com/kitsune-guuji/gvp"
         target="_blank"
-        class="opacity-50 hover:opacity-100"
+        class="hover:text-neutral-700 transition-colors ease-in-out duration-200"
       >
         <div class="flex flex-row gap-x-2 items-center">
-          <GitHubLogo class="w-6 h-auto inline-block" />
+          <GitHubLogo class="w-4 h-auto inline-block" />
           <p>GitHub</p>
         </div>
       </a>
 
-      <a href="#" class="opacity-50 hover:opacity-100">
+      <a href="#" class="hover:text-neutral-700 transition-colors ease-in-out duration-200">
         <div class="flex flex-row gap-x-2 items-center">
-          <DiscordLogo class="w-6 h-auto inline-block" />
+          <DiscordLogo class="w-4 h-auto inline-block" />
           <p>Discord</p>
         </div>
       </a>
