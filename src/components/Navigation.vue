ab74a8f04880bace892cacec494e34fe01e57827
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-26 23:38:39 +0400

    switch to popver navigation on mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,23 +1,12 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { computed, reactive } from "vue";
+import { computed } from "vue";
 import { RouterLink, useRouter } from "vue-router";
 
-import { MenuIcon, XCircleIcon } from "@heroicons/vue/outline";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+import { MenuIcon, XIcon } from "@heroicons/vue/outline";
 
 const router = useRouter();
-
-const store = reactive({
-  isOpen: false,
-});
-
-const openModal = () => {
-  store.isOpen = true;
-};
-
-const closeModal = () => {
-  store.isOpen = false;
-};
 
 const endpoints = computed(() => {
   return router.options.routes
@@ -34,49 +23,46 @@ const endpoints = computed(() => {
 </script>
 
 <template>
-  <!-- Mobile Modal Menu -->
-  <div
-    data-description="Mobile Navigation Modal"
-    :class="store.isOpen ? 'visible opacity-100' : 'invisible opacity-0'"
-    class="fixed flex flex-col gap-y-4 justify-center h-screen w-screen overflow-hidden bg-neutral-50 z-50"
-  >
-    <div class="mt-64 w-full flex flex-wrap justify-evenly">
-      <RouterLink
-        @click="closeModal"
-        v-for="endpoint in endpoints"
-        :key="endpoint.name"
-        :to="endpoint.path"
-        class="flex flex-col justify-center items-center p-6 m-2 rounded-lg aspect-square w-32"
-        active-class="!bg-primary-300 shadow-lg shadow-primary-300/50"
-      >
-        <component :is="endpoint.icon" class="w-8 h-8" />
-        <h1 class="font-semibold text-lg">{{ endpoint.name }}</h1>
-      </RouterLink>
-    </div>
-
-    <div @click="closeModal" class="self-center mt-16 cursor-pointer">
-      <XCircleIcon class="h-10 w-10 text-neutral-700" />
-    </div>
-  </div>
-
   <!-- 
     Mobile Navbar
     Note: select-none on nav is actually needed here otherwise it does some weird stuff on mobile firefox lol idk why
   -->
   <nav
     data-description="Mobile Bottom Navigation"
-    class="fixed lg:hidden bottom-0 h-14 w-full flex flex-row bg-neutral-50 border-t border-neutral-300 select-none z-10"
+    class="fixed lg:hidden bottom-0 h-14 w-full flex flex-row flex-wrap bg-neutral-100 border-t border-neutral-200 select-none z-10"
   >
     <div class="flex-grow self-center pl-6">
-      <h1 class="font-extrabold text-lg">GVP</h1>
+      <h1 class="font-extrabold text-lg">GENSHIN.ZENLESS</h1>
     </div>
 
-    <div
-      @click="openModal"
-      class="flex items-center px-6 cursor-pointer border-l border-neutral-300/30"
-    >
-      <MenuIcon class="w-6 h-6" />
-    </div>
+    <Popover v-slot="{ open }">
+      <PopoverButton as="div" class="h-full flex items-center px-6 cursor-pointer">
+        <component :is="open ? XIcon : MenuIcon" class="w-6 h-6" />
+      </PopoverButton>
+
+      <PopoverPanel as="div" class="fixed bottom-14 mb-4 mr-4 right-0 z-50">
+        <div class="bg-neutral-100 border border-neutral-200 rounded-lg p-2">
+          <div class="flex flex-col gap-y-2 text-lg font-semibold">
+            <RouterLink
+              v-for="endpoint in [...endpoints].reverse()"
+              :key="endpoint.name"
+              :to="endpoint.path"
+              class="px-3 py-2 rounded-lg"
+              active-class="bg-primary-500 text-white shadow-sm shadow-primary-300/50"
+            >
+              <div class="flex flex-row items-center gap-x-2">
+                <div class="flex-grow text-right">
+                  {{ endpoint.name }}
+                </div>
+                <div>
+                  <component :is="endpoint.icon" class="w-6 h-6" />
+                </div>
+              </div>
+            </RouterLink>
+          </div>
+        </div>
+      </PopoverPanel>
+    </Popover>
   </nav>
 
   <!-- Desktop Sidebard -->
@@ -86,7 +72,7 @@ const endpoints = computed(() => {
   >
     <div class="flex flex-col gap-y-2 sticky top-0 p-4 w-full">
       <div class="self-center py-4 mb-2 border-b border-neutral-200">
-        <h1 class="font-bold text-4xl">GVP</h1>
+        <h1 class="font-extrabold text-xl">GENSHIN.ZENLESS</h1>
       </div>
 
       <RouterLink
