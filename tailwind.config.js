62769f2c22b0662df214900c3fb5878621e2b21f
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-26 22:11:29 +0400

    change tailwind theme a little bit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,27 @@
 /* eslint-env node */
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
+
+// https://www.tailwindshades.com/
+
+const myColors = {
+  "purple-heart": {
+    DEFAULT: "#8A2BE2",
+    50: "#E3CDF8",
+    100: "#D9BBF6",
+    200: "#C597F1",
+    300: "#B273EC",
+    400: "#9E4FE7",
+    500: "#8A2BE2",
+    600: "#6E1ABB",
+    700: "#51138A",
+    800: "#340C59",
+    900: "#170527",
+  },
+};
+
+const myScreens = {
+  xmd: "896px",
+};
 
 module.exports = {
   content: ["./index.html", "./src/**/*.vue"],
@@ -7,10 +29,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.pink,
+        ...myColors,
+        primary: myColors["purple-heart"],
       },
       screens: {
-        xmd: "896px",
+        ...myScreens,
       },
     },
   },
