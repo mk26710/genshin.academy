e5baf19c96972a7ca0c3c972764734c8fb3c6a9b
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-28 19:22:56 +0400

    convert guides data to Vue SFCs

---
+++ src/data/guides/characters/kamisato_ayaka.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Kamisato Ayaka</h1>
+  <p>The best girl of all</p>
+</template>
---
+++ src/data/guides/characters/yae_miko.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Yae Miko</h1>
+
+  <p>One of the best girls out there!</p>
+</template>
