34d246cc5fc449a21baa2b31e071e4418d431cee
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-28 23:31:07 +0400

    I'm so done trying to figure out dynamic sfcs..
    ...so let's go with jsons instead :)

--- src/data/guides/characters/kamisato_ayaka.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <h1>Kamisato Ayaka</h1>
-  <p>The best girl of all</p>
-</template>
--- src/data/guides/characters/yae_miko.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1>Yae Miko</h1>
-
-  <p>One of the best girls out there!</p>
-</template>
