f354564b7c1dfee11e768704079a0660148e1e2f
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-28 23:49:36 +0400

    template error page

---
+++ src/components/Error.vue
@@ -0,0 +1,34 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { isString } from "lodash-es";
+import { RouterLink } from "vue-router";
+
+import { ChevronDoubleLeftIcon } from "@heroicons/vue/solid";
+
+interface Props {
+  title: string;
+  description?: string;
+  buttonTitle: string;
+  buttonHref: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-full w-full">
+    <div class="flex flex-col gap-y-3">
+      <h1 class="text-xl font-bold">{{ title }}</h1>
+
+      <p v-if="isString(description)">{{ description }}</p>
+
+      <RouterLink
+        :to="buttonHref"
+        class="mt-8 px-3 py-2 bg-primary-500 shadow-lg shadow-primary-500/50 text-white text-lg font-bold rounded-lg flex flex-row justify-center items-center gap-x-2"
+      >
+        <ChevronDoubleLeftIcon class="w-5 h-5" />
+        <span>{{ buttonTitle }}</span>
+      </RouterLink>
+    </div>
+  </div>
+</template>
