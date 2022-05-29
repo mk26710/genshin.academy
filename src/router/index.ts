95ff8a48c67baa836850dc64098ec5d8964435f1
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-29 21:45:47 +0400

    (cloudflare) non hash history with cf pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
@@ -61,7 +61,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
