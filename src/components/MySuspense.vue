93aa1b061c59db93af732c2d3b47738fa79c6cf5
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-29 22:52:01 +0400

    custom suspense to use with v-if

---
+++ src/components/MySuspense.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import SuspenseFallback from "./SuspenseFallback.vue";
+
+interface Props {
+  value: boolean;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <slot v-if="value" />
+  <SuspenseFallback v-else />
+</template>
