77e4ee43dc06ec1fa682635eabf5356f7f044f6b
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-02 20:21:06 +0400

    prepare character card

---
+++ src/components/Cards/CharacterCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+
+import type { Rarity } from "@/data/types/genshin";
+import { characterIcon } from "@/lib/helpers";
+
+interface Props {
+  id: string;
+  name: string;
+  rarity: Rarity;
+}
+
+const props = defineProps<Props>();
+
+const icon = characterIcon(props.id);
+</script>
+
+<template>
+  <RouterLink :to="`/character/${id}`">
+    <div>
+      <img :src="icon" />
+    </div>
+    <div>{{ name }}</div>
+  </RouterLink>
+</template>
--- src/views/CharactersView.vue
+++ src/views/CharactersView.vue
@@ -1,32 +1,12 @@
 <script setup lang="ts">
+import CharacterCard from "../components/Cards/CharacterCard.vue";
 import MainContainer from "@/components/MainContainer.vue";
 
 import characters from "@/data/characters";
-import { Rarity } from "@/data/types/genshin";
-import { characterIcon } from "@/lib/helpers";
-
-const bgColor = (rarity: Rarity) => {
-  if (rarity === Rarity.FIVE_STAR) {
-    return "bg-orange-300";
-  } else if (rarity === Rarity.FOUR_STAR) {
-    return "bg-purple-300";
-  }
-
-  return "bg-neutral-400";
-};
 </script>
 
 <template>
   <main-container>
-    <div v-for="character in characters" :key="character.id" class="flex flex-row">
-      <div :class="bgColor(character.rarity)" class="flex flex-col">
-        <div>
-          <img :src="characterIcon(character.id)" />
-        </div>
-        <div class="self-center">
-          {{ character.name }}
-        </div>
-      </div>
-    </div>
+    <CharacterCard v-for="character in characters" :key="character.id" v-bind="character" />
   </main-container>
 </template>
