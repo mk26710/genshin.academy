911b94d44292e8a42b9a29f2fe7b0811827fac71
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-02 19:30:55 +0400

    genshin types

---
+++ src/data/types/genshin.ts
@@ -0,0 +1,25 @@
+export enum Rarity {
+  FIVE_STAR = 5,
+  FOUR_STAR = 4,
+  THREE_STAR = 3,
+  TWO_STAR = 2,
+  ONE_STAR = 1,
+  ZERO_STAR = 0,
+}
+
+export enum WeaponType {
+  SWORD,
+  CLAYMORE,
+  POLEARM,
+  CATALYST,
+  BOW,
+}
+
+export enum Vision {
+  CRYO,
+  ELECTRO,
+  PYRO,
+  ANEMO,
+  HYDRO,
+  GEO,
+}
