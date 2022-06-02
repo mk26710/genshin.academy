f776ed78ca4f3522a7df3a2eed1ba1b86776c393
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-02 20:02:59 +0400

    temp solution for characters list page

--- src/views/CharactersView.vue
+++ src/views/CharactersView.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
 import MainContainer from "@/components/MainContainer.vue";
+
+import characters from "@/data/characters";
+import { Rarity } from "@/data/types/genshin";
+import { characterIcon } from "@/lib/helpers";
+
+const bgColor = (rarity: Rarity) => {
+  if (rarity === Rarity.FIVE_STAR) {
+    return "bg-orange-300";
+  } else if (rarity === Rarity.FOUR_STAR) {
+    return "bg-purple-300";
+  }
+
+  return "bg-neutral-400";
+};
 </script>
 
 <template>
-  <main-container> ehehe :) </main-container>
+  <main-container>
+    <div v-for="character in characters" :key="character.id" class="flex flex-row">
+      <div :class="bgColor(character.rarity)" class="flex flex-col">
+        <div>
+          <img :src="characterIcon(character.id)" />
+        </div>
+        <div class="self-center">
+          {{ character.name }}
+        </div>
+      </div>
+    </div>
+  </main-container>
 </template>
