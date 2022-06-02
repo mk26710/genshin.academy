1d65e2e790225f02f0b56d3dca2c3128bb694689
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-02 19:49:42 +0400

    helper function to get icon path

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -4,3 +4,10 @@
 export const avatarHeaderPath = (id: string, extension = "png") => {
   return `/img/characters/${id}/avatar_header.${extension}`;
 };
+
+/**
+ * Builds url to character's icon
+ */
+export const characterIcon = (id: string) => {
+  return `/img/characters/${id}/icon.png`;
+};
