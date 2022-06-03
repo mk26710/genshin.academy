3d746aae48042a0dbfc2fc6d86180343fdd144a4
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-03 20:59:41 +0400

    (cloudflare) changed loading cloud to spinner

--- src/components/SuspenseFallback.vue
+++ src/components/SuspenseFallback.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts">
-import { CloudDownloadIcon } from "@heroicons/vue/outline";
-</script>
-
 <template>
-  <div class="flex flex-col items-center justify-center w-full h-full">
-    <CloudDownloadIcon class="animate-bounce w-12 h-12" />
-    <h1 class="font-bold text-2xl">Loading</h1>
-  </div>
+  <Transition name="fade" mode="out-in" appear>
+    <div class="flex flex-col items-center justify-center w-full h-full">
+      <div>
+        <div
+          style="border-top-color: transparent"
+          class="w-16 h-16 border-8 border-primary-500 dark:border-dark-200 border-solid rounded-full animate-spin"
+        ></div>
+      </div>
+    </div>
+  </Transition>
 </template>
