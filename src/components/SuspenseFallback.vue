dc5149f42fa83fea5b70c759f7ff14faa6d57430
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-03 19:26:50 +0400

    removed main container from fallback

--- src/components/SuspenseFallback.vue
+++ src/components/SuspenseFallback.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import MainContainer from "@/components/MainContainer.vue";
 import { CloudDownloadIcon } from "@heroicons/vue/outline";
 </script>
+
 <template>
-  <MainContainer>
-    <div class="flex flex-col items-center justify-center w-full h-full">
-      <CloudDownloadIcon class="animate-bounce w-12 h-12" />
-      <h1 class="font-bold text-2xl">Loading</h1>
-    </div>
-  </MainContainer>
+  <div class="flex flex-col items-center justify-center w-full h-full">
+    <CloudDownloadIcon class="animate-bounce w-12 h-12" />
+    <h1 class="font-bold text-2xl">Loading</h1>
+  </div>
 </template>
