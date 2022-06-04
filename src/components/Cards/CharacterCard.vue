8aa85db717f9f9d9341bbac77da917d0266fa202
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-04 21:59:13 +0400

    updated character card styling

--- src/components/Cards/CharacterCard.vue
+++ src/components/Cards/CharacterCard.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
 
-import type { Rarity } from "@/data/types/genshin";
+import { Rarity } from "@/data/types/genshin";
 import { characterIcon } from "@/lib/helpers";
+import { computed } from "vue";
 
 interface Props {
   id: string;
@@ -13,13 +14,29 @@ interface Props {
 const props = defineProps<Props>();
 
 const icon = characterIcon(props.id);
+
+const iconBg = computed(() => {
+  if (props.rarity === Rarity.FIVE_STAR) {
+    return "from-[#945C2C] to-[#B27330]";
+  } else if (props.rarity === Rarity.FOUR_STAR) {
+    return "from-[#5E5789] to-[#9C75B7]";
+  }
+
+  return "from-[#6A6D74] to-[#868586]";
+});
 </script>
 
 <template>
-  <RouterLink :to="`/character/${id}`">
-    <div>
-      <img :src="icon" :alt="`${id} icon`" />
+  <RouterLink
+    :to="`/character/${id}`"
+    :class="iconBg"
+    class="w-32 rounded-lg bg-neutral-100 dark:bg-dark-900/40 border border-neutral-200 dark:border-dark-200/10"
+  >
+    <div class="w-full aspect-square rounded-t-lg bg-gradient-to-b">
+      <img class="aspect-square w-full text-transparent" :src="icon" :alt="`${id} icon`" />
     </div>
-    <div>{{ name }}</div>
+    <div class="w-full">
+      <p class="p-1 text-sm text-center">{{ name }}</p>
+    </div>
   </RouterLink>
 </template>
