6421e6969f373a5ac9be92ba92c8ebbd68b6756b
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-04 19:17:48 +0400

    seems like webstorm wants this imported

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, RouterView } from "vue-router";
 
 import MainContainer from "./components/MainContainer.vue";
 import Footer from "@/components/Footer.vue";
