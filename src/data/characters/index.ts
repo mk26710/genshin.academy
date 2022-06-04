9cd5dae35409cb7214c7bd8bf19ff70cf03d54d3
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-04 21:58:47 +0400

    index file of characters data now more flexible

--- src/data/characters/index.ts
+++ src/data/characters/index.ts
@@ -1,11 +1,15 @@
+import type { Character } from "@/data/types/character";
+
 import yae_miko from "@/data/characters/yae_miko";
 import kamisato_ayaka from "@/data/characters/kamisato_ayaka";
 
-export const characters = {
-  yae_miko,
-  kamisato_ayaka,
-};
+const _array = [yae_miko, kamisato_ayaka];
 
-export const ids = Object.keys(characters);
-
-export default characters;
+export const charactersArray = [..._array].sort((a, b) => b.rarity - a.rarity);
+export const charactersMap = charactersArray.reduce<Map<string, Character>>((map, current) => {
+  map.set(current.id, current);
+  return map;
+}, new Map());
+export const charactersIDs = charactersArray.reduce<string[]>((arr, current) => {
+  return [...arr, current.id];
+}, []);
