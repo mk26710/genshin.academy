6ead426b5cef7ae010b1815890e4ae870c197fb1
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-10 22:09:19 +0400

    sort by rarity and names

--- src/data/characters/index.ts
+++ src/data/characters/index.ts
@@ -6,7 +6,9 @@ import yelan from "@/data/characters/yelan";
 
 const _array = [yae_miko, kamisato_ayaka, yelan];
 
-export const charactersArray = [..._array].sort((a, b) => b.rarity - a.rarity);
+export const charactersArray = [..._array].sort(
+  (a, b) => a.name.localeCompare(b.name) || b.rarity - a.rarity,
+);
 
 export const charactersMap = charactersArray.reduce<Map<string, Character>>((map, current) => {
   map.set(current.id, current);
