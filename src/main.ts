b588be4ff7789a5e4fb5f0d9675623565a66c324
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-12 19:25:17 +0400

    import cards styles

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import router from "@/router";
 import "@/assets/styles/main.scss";
 import "@/assets/styles/markdown.scss";
 import "@/assets/styles/calculator.scss";
+import "@/assets/styles/cards.scss";
 
 const app = createApp(App);
 
