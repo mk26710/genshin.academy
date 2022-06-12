def6fa3eed6092045b9877904c4414dac9fb459a
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-12 17:22:22 +0400

    local time formats should be preferred

--- src/plugins/dayjs.ts
+++ src/plugins/dayjs.ts
@@ -1,10 +1,13 @@
 import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
 import type { App } from "vue";
 
 import { dayjsKey } from "@/plugins/symbols";
 
 export default {
   install: (app: App) => {
+    dayjs.extend(localizedFormat);
+
     app.config.globalProperties.$dayjs = dayjs;
     app.provide(dayjsKey, dayjs);
   },
