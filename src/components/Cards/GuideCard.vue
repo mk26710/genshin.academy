146a893078b1dd5582ca4326a9b3fc6e7da2ecba
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-12 19:11:22 +0400

    moved cards styling to a file

--- src/components/Cards/GuideCard.vue
+++ src/components/Cards/GuideCard.vue
@@ -19,27 +19,27 @@ const rootProps = computed(() => (isString(props.to) ? { to: props.to } : {}));
 </script>
 
 <template>
-  <component
-    :is="rootComponent"
-    v-bind="rootProps"
-    class="flex flex-col text-neutral-900 dark:text-neutral-300 bg-neutral-100 dark:bg-dark-850 border border-neutral-200 dark:border-neutral-200/10 rounded-lg"
-  >
-    <div class="min-h-[10rem] lg:min-h-[20rem] w-full aspect-[9/16]">
-      <img
-        v-if="!!thumbnail"
-        :src="thumbnail"
-        loading="lazy"
-        class="object-fill object-top rounded-t-lg"
-      />
-    </div>
-    <div class="px-6 py-4 space-y-2">
-      <div v-if="!!publishedAt" class="font-medium text-sm leading-6 text-primary-500">
-        {{ $dayjs(publishedAt).format("HH:mm on DD.MM.YYYY") }}
+  <component :is="rootComponent" v-bind="rootProps" class="card card-vertical">
+    <img
+      v-if="!!thumbnail"
+      :src="thumbnail"
+      loading="lazy"
+      class="card-thumbnail min-h-[10rem] lg:min-h-[20rem] aspect-[9/16]"
+    />
+
+    <div class="card-text-container">
+      <div
+        v-if="!!publishedAt"
+        class="card-text-container-item font-medium text-sm leading-6 text-primary-500"
+      >
+        {{ $dayjs(publishedAt).format("lll") }}
       </div>
-      <p class="block font-semibold text-base leading-6">
+
+      <p class="card-text-container-item block font-semibold text-base leading-6">
         {{ title }}
       </p>
-      <p v-if="!!description" class="text-sm text-justify leading-6">
+
+      <p v-if="!!description" class="card-text-container-item text-sm text-justify leading-6">
         {{ description }}
       </p>
     </div>
