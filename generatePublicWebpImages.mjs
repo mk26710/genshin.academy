5e79b593794633e7a70355be315752c253d5bf32
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-15 20:59:07 +0400

    more flexible webp generation

--- generatePublicWebpImages.mjs
+++ generatePublicWebpImages.mjs
@@ -1,18 +1,32 @@
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
 import { globby } from "globby";
 import sharp from "sharp";
+import { Command } from 'commander';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const fileExists = async (path) => !!(await fs.promises.stat(path).catch(e => false));
+
+const program = new Command();
+program
+  .option("-r", "--regenerate", false)
+  .parse(process.argv);
+
 const pattern = path.join(__dirname, "./public", "**/*.{png,jpg,jpeg}");
 const paths = await globby(pattern);
 
-paths.forEach(async (p) => {
+for (const p of paths) {
   const webpFileName = path.basename(p, path.extname(p)) + ".webp";
   const webpFilePath = path.join(path.dirname(p), webpFileName);
+
+  if (fileExists(webpFilePath) && !program.opts().r) {
+    console.log(`* Skipping ${webpFileName} since it already exists at ${webpFilePath}`);
+    continue;
+  }
 
   console.log(`* generating ${webpFilePath}...`);
 
@@ -23,4 +37,6 @@ paths.forEach(async (p) => {
   } catch (e) {
     console.error(`SKIP - ERROR WHILE PROCESSING ${p} - ${e}`);
   }
-});
+}
+
+console.log("> finish")
