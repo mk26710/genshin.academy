bf0529e11952ebeb240152b55378d459d168dbd3
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-15 21:46:24 +0400

    display some information on character pages

--- src/views/characters/CharactersId.vue
+++ src/views/characters/CharactersId.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+import { isNil } from "lodash-es";
+
 import MainContainer from "@/components/MainContainer.vue";
+
+import { avatarPath } from "@/lib/helpers";
+import { charactersMap } from "@/data/characters";
+
+const router = useRouter();
+const route = useRoute();
+
+const character = charactersMap.get(`${route.params.id}`);
+if (isNil(character)) {
+  router.push("/404");
+}
 </script>
 
 <template>
   <MainContainer>
-    <h1>yes</h1>
+    <template v-if="!isNil(character)">
+      <h1 class="font-semibold text-4xl mb-4 mt-6">{{ character.name }}</h1>
+      <p>{{ character.description }}</p>
+      <img :src="avatarPath(character.id, 'webp')" />
+    </template>
   </MainContainer>
 </template>
-
-<style scoped></style>
