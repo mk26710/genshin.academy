b698bf3731e4298186c4d0ad28da1b79a263c504
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-18 18:37:46 +0400

    updated character schema
     - using Object.freeze() on all sub-schemas for better usability of the schemas

--- src/data/character.ts
+++ src/data/character.ts
@@ -2,43 +2,40 @@ import { z } from "zod";
 import type { Component } from "vue";
 
 import { Rarity, Vision, Weapon } from "@/data/types/genshin";
-import { deepFreezeTranformer } from "@/lib/utils";
+import { freezeTransform } from "@/lib/utils";
 
-export const BirthdayDate = z.tuple([
-  z.number().int().min(1).max(31),
-  z.number().int().min(1).max(12),
-]);
+export const BirthdayDate = z
+  .tuple([z.number().int().min(1).max(31), z.number().int().min(1).max(12)])
+  .transform(freezeTransform);
 
 export type BirthdayDate = z.infer<typeof BirthdayDate>;
 
-export const Constellation = z.object({
-  level: z.number().min(1).max(6),
-  title: z.string(),
-  description: z.string(),
-  icon: z.union([z.custom<Component>(), z.string()]).optional(),
-});
+export const Constellation = z
+  .object({
+    level: z.number().min(1).max(6),
+    title: z.string(),
+    description: z.string(),
+    icon: z.union([z.custom<Component>(), z.string()]).optional(),
+  })
+  .transform(freezeTransform);
 
 export type Constellation = z.infer<typeof Constellation>;
 
-export const StoryEntry = z.object({
-  title: z.string(),
-  description: z.string(),
-});
+export const StoryEntry = z
+  .object({
+    title: z.string(),
+    description: z.string(),
+  })
+  .transform(freezeTransform);
 
 export type StoryEntry = z.infer<typeof StoryEntry>;
 
 export const Character = z
   .object({
-    /** Name of the character that is used for filenames, paths etc. related to the character */
     id: z.string().regex(/[a-z_]+/g),
-
-    /** Human-readable name of the character */
     name: z.string(),
-
-    /** Official description of the character from the game */
     description: z.string(),
-
-    /** An array of data related to constellations */
+    birthday: BirthdayDate,
     constellations: z.tuple([
       Constellation,
       Constellation,
@@ -47,15 +44,11 @@ export const Character = z
       Constellation,
       Constellation,
     ]),
-
-    /** Storylines from the game (character -> profile -> story) */
     story: StoryEntry.array(),
-
     rarity: z.nativeEnum(Rarity),
     weapon: z.nativeEnum(Weapon),
     vision: z.nativeEnum(Vision),
-    birthday: BirthdayDate,
   })
-  .transform(deepFreezeTranformer);
+  .transform(freezeTransform);
 
 export type Character = z.infer<typeof Character>;
