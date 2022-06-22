bc9d5baba35db00f92f70491ee2378dd33fe4eb5
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-22 20:39:09 +0400

    guide display page

---
+++ pages/guides/[id].vue
@@ -0,0 +1,34 @@
+<template>
+  <MainContainer>
+    <section class="md-body" v-if="!isNil(guide)" v-html="guide.html" />
+    <ErrorDisplay
+      v-else
+      title="Hey, this guide doesn't exist!"
+      :description="`Guide for ${id} was not found.`"
+      button-title="Back to Guides"
+      button-href="/guides"
+    />
+  </MainContainer>
+</template>
+
+
+<script setup lang="ts">
+import { onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import { isNil } from "lodash-es";
+
+import MainContainer from "@/components/MainContainer.vue";
+import ErrorDisplay from "@/components/ErrorDisplay.vue";
+
+import { useGuideStore } from "@/stores/guide";
+
+const route = useRoute();
+const id = route.params.id.toString();
+
+const { getGuideById } = useGuideStore();
+const guide = getGuideById(id);
+
+onBeforeMount(() => {
+  window.scrollTo({ top: 0 });
+});
+</script>
