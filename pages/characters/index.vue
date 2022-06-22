6be5158328d3c84c583e043a5e4b9791dfe78523
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-22 21:01:01 +0400

    characters pages

---
+++ pages/characters/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <MainContainer>
+    <h1 class="font-semibold text-4xl mb-4 mt-6">Characters</h1>
+
+    <div class="flex flex-row flex-wrap gap-2 justify-evenly md:justify-start">
+      <CharacterCard
+        v-for="character in charactersArray"
+        :key="character.id"
+        :id="character.id"
+        :name="character.name"
+        :rarity="character.rarity"
+      />
+    </div>
+  </MainContainer>
+</template>
+
+<script setup lang="ts">
+import { charactersArray } from "@/data/characters";
+</script>
