89690a627cf5f70cb497f184b09b0e7ec2ed5b26
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-22 21:01:01 +0400

    characters pages

---
+++ pages/characters/[id].vue
@@ -0,0 +1,31 @@
+<template>
+  <MainContainer>
+    <template v-if="!isNil(character)">
+      <h1 class="font-semibold text-4xl mb-4 mt-6">{{ character.name }}</h1>
+      <p>{{ character.description }}</p>
+
+      <Image :src="avatarPath(character.id)" />
+    </template>
+    <ErrorDisplay
+      v-else
+      button-href="/characters"
+      button-title="Back to all characters"
+      title="Character data not found"
+    />
+  </MainContainer>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { isNil } from "lodash-es";
+
+import { avatarPath } from "@/lib/helpers";
+import { getCharacterById } from "@/data/characters";
+import type { Character } from "@/data/character";
+
+const route = useRoute();
+
+const id = route.params.id.toString();
+const character = ref<Character | undefined>(getCharacterById(id));
+</script>
