311bfc34c82dc1506e88845773acf80dfcca40a9
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-22 21:24:11 +0400

    aded characters page to navigation

--- pages/characters/index.vue
+++ pages/characters/index.vue
@@ -3,7 +3,7 @@
     <h1 class="font-semibold text-4xl mb-4 mt-6">Characters</h1>
 
     <div class="flex flex-row flex-wrap gap-2 justify-evenly md:justify-start">
-      <CharacterCard
+      <CardsCharacter
         v-for="character in charactersArray"
         :key="character.id"
         :id="character.id"
@@ -16,4 +16,11 @@
 
 <script setup lang="ts">
 import { charactersArray } from "@/data/characters";
+import { StarIcon } from "@heroicons/vue/outline";
+
+definePageMeta({
+  name: "Characters",
+  icon: StarIcon,
+  navbar: true,
+});
 </script>
