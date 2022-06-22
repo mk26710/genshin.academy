fc71d766b3e478db79d7af003f272da44d33f922
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-22 20:38:01 +0400

    fetch guides on app start

--- app.vue
+++ app.vue
@@ -7,3 +7,15 @@
     </div>
   </Body>
 </template>
+
+<script setup lang="ts">
+import { useGuideStore } from "~/stores/guide";
+
+const guide = useGuideStore();
+
+onBeforeMount(async () => {
+  if (!guide.isUpdated) {
+    await guide.update();
+  }
+});
+</script>
