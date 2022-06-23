f9b1b5c28ec127eab278f9f3fd8f51e62807be0b
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-23 18:28:55 +0400

    (cloudflare) should have used NuxtLink on 404

--- pages/404.vue
+++ pages/404.vue
@@ -5,13 +5,13 @@
       <p>You are trying to visit a non existing page :(</p>
 
       <div class="mt-12">
-        <router-link to="/">
+        <NuxtLink to="/">
           <button
             class="rounded-lg bg-primary-600 hover:shadow-lg hover:shadow-primary-600/30 text-neutral-100 py-2 px-3 transition-all duration-200 ease-in-out font-bold"
           >
             Click to go home
           </button>
-        </router-link>
+        </NuxtLink>
       </div>
     </div>
   </MainContainer>
