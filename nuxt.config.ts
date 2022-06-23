6fe909cd14d92e32c62c8feddb7120d1ffff6804
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-23 13:25:22 +0400

    colromode and vueuse nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,12 @@ export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/color-mode", "@vueuse/nuxt"],
+  colorMode: {
+    classSuffix: "",
+    storageKey: "theme",
+  },
   css: ["@/assets/styles/main.scss"],
-  ssr: false,
-  target: "static",
+  // ssr: false,
+  // target: "static",
 });
