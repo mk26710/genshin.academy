127fc53eb46d225ae478973d48b8be4ba9afe686
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-23 17:12:51 +0400

    guide display meta and redundant imports

--- pages/guides/[id].vue
+++ pages/guides/[id].vue
@@ -17,18 +17,37 @@ import { onBeforeMount } from "vue";
 import { useRoute } from "vue-router";
 import { isNil } from "lodash-es";
 
-import MainContainer from "@/components/MainContainer.vue";
-import ErrorDisplay from "@/components/ErrorDisplay.vue";
-
 import { useGuideStore } from "@/stores/guide";
+import { getCharacterById } from "~~/data/characters";
 
 const route = useRoute();
 const id = route.params.id.toString();
 
 const { getGuideById } = useGuideStore();
+
+const character = getCharacterById(id);
 const guide = getGuideById(id);
 
 onBeforeMount(() => {
   window.scrollTo({ top: 0 });
+});
+
+useHead(() => {
+  if (character == null) {
+    return {};
+  }
+
+  return {
+    title: `${character.name} Guide`,
+    meta: [
+      { property: "og:title", content: character.name },
+      { name: "description", content: character.description },
+      { property: "og:description", content: character.description },
+      {
+        property: "og:image",
+        content: `https://genshin.zenless.club/img/characters/${character.id}/icon.png`,
+      },
+    ],
+  };
 });
 </script>
