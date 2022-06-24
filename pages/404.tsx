3aa44a906353421bf6f5a48b4c2c247b4e614e32
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-24 16:31:59 +0400

    WIP custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import { MainLayout } from "@/components/MainLayout";
+import { NextPage } from "next";
+
+const NotFound: NextPage = () => {
+  return <MainLayout verticalCenter>404 NOT FOUND</MainLayout>;
+};
+
+export default NotFound;
