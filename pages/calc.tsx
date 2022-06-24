21115f36ce4b0d1108de6cf2f64a141585311c76
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-24 18:57:53 +0400

    added layout and container on calculators

--- pages/calc.tsx
+++ pages/calc.tsx
@@ -1,5 +1,17 @@
+import { Container } from "@/components/Container";
+import { Layout } from "@/components/Layout";
+
 const CalcPage = () => {
-  return <>1</>;
+  return (
+    <Layout
+      title="Calculators"
+      description="Genshin Impact calculators for crti value, resin and etc."
+    >
+      <Container>
+        <h1>WIP</h1>
+      </Container>
+    </Layout>
+  );
 };
 
 export default CalcPage;
