7ea546b03e85c93cb8d5e5faf081f268823b96a8
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-24 23:14:31 +0400

    added progress bar on navigations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/router";
+import { useEffectOnce } from "react-use";
 
+import nProgress from "nprogress";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 
@@ -19,12 +21,40 @@ import "@/assets/styles/markdown.scss";
 import "@/assets/styles/nprogress.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
+  const router = useRouter();
+
+  useEffectOnce(() => {
     dayjs.extend(localizedFormat);
     dayjs.locale(navigator.language);
 
     console.log("dayjs extended with localized formats");
-  }, []);
+
+    nProgress.configure({
+      showSpinner: true,
+    });
+
+    console.log("nProgress configured");
+
+    const handleStart = () => {
+      nProgress.start();
+    };
+
+    const handleStop = () => {
+      setTimeout(() => {
+        nProgress.done();
+      }, 100);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  });
 
   return (
     <ThemeProvider attribute="class">
