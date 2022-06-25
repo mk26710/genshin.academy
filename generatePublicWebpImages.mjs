e921aa181c4d3342b1993a14143a13d155a5d63b
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-25 19:51:24 +0400

    fix webp generation script

--- generatePublicWebpImages.mjs
+++ generatePublicWebpImages.mjs
@@ -1,31 +1,38 @@
 /* eslint-env node */
-import fs from "fs";
-import path from "path";
+import { access } from "fs/promises";
+import { constants } from "fs";
+import {basename, join, extname, dirname, } from "path";
 import { fileURLToPath } from "url";
-
 import { globby } from "globby";
 import sharp from "sharp";
 import { Command } from "commander";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const fileExists = async (path) => !!(await fs.promises.stat(path).catch((e) => false));
+const __dirname = dirname(__filename);
 
 const program = new Command();
 program.option("-r", "--regenerate", false).parse(process.argv);
 
-const pattern = path.join(__dirname, "./public", "**/*.{png,jpg,jpeg}");
+const pattern = join(__dirname, "./public", "**/*.{png,jpg,jpeg}");
 const paths = await globby(pattern);
 
-for (const p of paths) {
-  const webpFileName = path.basename(p, path.extname(p)) + ".webp";
-  const webpFilePath = path.join(path.dirname(p), webpFileName);
-
-  if (fileExists(webpFilePath) && !program.opts().r) {
-    console.log(`* Skipping ${webpFileName} since it already exists at ${webpFilePath}`);
-    continue;
+const fileExists = async (file) => {
+  try{
+    await access(file, constants.F_OK)
+    return true;
+  } catch {
+    return false;
   }
+};
+
+for (const p of paths) {
+  const webpFileName = basename(p, extname(p)) + ".webp";
+  const webpFilePath = join(dirname(p), webpFileName);
+
+    if (await fileExists(webpFilePath) && program.opts().r !== true) {
+      console.log(`* Skipping ${webpFileName} since it already exists at ${webpFilePath}`);
+      continue;
+    }
 
   console.log(`* generating ${webpFilePath}...`);
 
