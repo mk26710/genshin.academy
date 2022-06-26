c1a3cc0faf8cd0a5f5529ba6e7bac60ad26160f3
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-26 19:54:47 +0400

    fix eslint again

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
     "vue/setup-compiler-macros": true,
   },
   rules: {
+    "vue/multi-word-component-names": `off`,
     "@typescript-eslint/no-unused-vars": [`error`],
     quotes: [`warn`, `backtick`],
   },
