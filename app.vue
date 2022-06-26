f76b67adbd047476db6df3e79707930bb87cb727
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-26 20:28:46 +0400

    redundant, every page will use useZenlessMeta()

--- app.vue
+++ app.vue
@@ -11,20 +11,6 @@
 <script setup lang="ts">
 import { useGuideStore } from "~/stores/guide";
 
-const description = `Genshin Impact chracters, calculators, guides and more!`;
-
-useHead({
-  titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} - GENSHIN.ZENLESS` : "GENSHIN.ZENLESS.CLUB";
-  },
-  meta: [
-    { property: "og:title", content: "GENSHIN.ZENLESS.CLUB" },
-    { property: "og:type", content: "website" },
-    { name: "description", content: description },
-    { property: "og:description", content: description },
-  ],
-});
-
 const guide = useGuideStore();
 
 onBeforeMount(async () => {
