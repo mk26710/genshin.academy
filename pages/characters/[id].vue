9135acd2aad5e772df63038dc689b7cc64137408
BinaryKitsune <103903444+BinaryKitsune@users.noreply.github.com>
2022-06-27 16:58:09 +0400

    redirect to /404 if character doesnt exist

--- pages/characters/[id].vue
+++ pages/characters/[id].vue
@@ -1,24 +1,15 @@
 <template>
   <MainContainer>
-    <template v-if="!isNil(character)">
+    <template v-if="character != null">
       <h1 class="font-semibold text-4xl mb-4 mt-6">{{ character.name }}</h1>
       <p>{{ character.description }}</p>
 
       <Image :src="avatarPath(character.id)" />
     </template>
-    <ErrorDisplay
-      v-else
-      button-href="/characters"
-      button-title="Back to all characters"
-      title="Character data not found"
-    />
   </MainContainer>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from "vue-router";
-import { isNil } from "lodash-es";
-
 import { avatarPath } from "@/lib/helpers";
 import { getCharacterById } from "@/data/characters";
 
@@ -26,6 +17,10 @@ const route = useRoute();
 
 const id = route.params.id.toString();
 const character = getCharacterById(id);
+
+if (character == null) {
+  await navigateTo(`/404`);
+}
 
 useZenlessMeta(() => {
   if (character == null) {
