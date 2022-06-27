58a9a3ba9243696ade5a147162c35c1d004a2a9f
BinaryKitsune <103903444+BinaryKitsune@users.noreply.github.com>
2022-06-27 15:52:27 +0400

    fix generation scripts formatting

--- generatePublicWebpImages.mjs
+++ generatePublicWebpImages.mjs
@@ -1,7 +1,7 @@
 /* eslint-env node */
 import { access } from "fs/promises";
 import { constants } from "fs";
-import {basename, join, extname, dirname, } from "path";
+import { basename, join, extname, dirname } from "path";
 import { fileURLToPath } from "url";
 import { globby } from "globby";
 import sharp from "sharp";
@@ -11,14 +11,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const program = new Command();
-program.option("-r", "--regenerate", false).parse(process.argv);
+program.option(`-r`, `--regenerate`, false).parse(process.argv);
 
-const pattern = join(__dirname, "./public", "**/*.{png,jpg,jpeg}");
+const pattern = join(__dirname, `./public`, `**/*.{png,jpg,jpeg}`);
 const paths = await globby(pattern);
 
 const fileExists = async (file) => {
-  try{
-    await access(file, constants.F_OK)
+  try {
+    await access(file, constants.F_OK);
     return true;
   } catch {
     return false;
@@ -26,21 +26,21 @@ const fileExists = async (file) => {
 };
 
 for (const p of paths) {
-  const webpFileName = basename(p, extname(p)) + ".webp";
+  const webpFileName = basename(p, extname(p)) + `.webp`;
   const webpFilePath = join(dirname(p), webpFileName);
 
-    if (await fileExists(webpFilePath) && program.opts().r !== true) {
-      console.log(`* Skipping ${webpFileName} since it already exists at ${webpFilePath}`);
-      continue;
-    }
+  if ((await fileExists(webpFilePath)) && program.opts().r !== true) {
+    console.log(`* Skipping ${webpFileName} since it already exists at ${webpFilePath}`);
+    continue;
+  }
 
   console.log(`* generating ${webpFilePath}...`);
 
   try {
-    await sharp(p).webp({ nearLossless: true }).toFile(webpFilePath);
+    await sharp(p).webp({ lossless: true }).toFile(webpFilePath);
   } catch (e) {
     console.error(`SKIP - ERROR WHILE PROCESSING ${p} - ${e}`);
   }
 }
 
-console.log("> finish");
+console.log(`> finish`);
