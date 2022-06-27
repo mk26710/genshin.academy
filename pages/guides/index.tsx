66c7d8481007a2c446d0254f3f8962102ea03974
BinaryKitsune <103903444+BinaryKitsune@users.noreply.github.com>
2022-06-27 22:18:49 +0400

    fixed usEffect deps and other problems
     - switched to hook instead of wrapper component for router ready
     - input will inherit query values

--- pages/guides/index.tsx
+++ pages/guides/index.tsx
@@ -1,55 +1,66 @@
-import { type ChangeEvent, useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import { NextPage } from "next";
 import { debounce, isEmpty } from "lodash-es";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { type ChangeEvent, useEffect, useRef } from "react";
 
-import { Layout } from "@/components/Layout";
 import { GuideCard } from "@/components/cards/GuideCard";
 import { Container } from "@/components/Container";
-import { RouterReady } from "@/components/RouterReady";
-
+import { Layout } from "@/components/Layout";
 import { charactersArray } from "@/data/characters";
 import published from "@/data/guides/compiled/characters/published.json";
+import { useRouterReady } from "@/hooks/useRouterReady";
 
 // this doesn't need to be reactive
 const publishedCharacters = charactersArray.filter(({ id }) => published.includes(id));
 
 const GuidesIndex: NextPage = () => {
   const router = useRouter();
+  const ready = useRouterReady();
 
-  const { q = "" } = router.query;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const q = router.query.q?.toString() ?? "";
 
   const handleChange = debounce((e: ChangeEvent<HTMLInputElement>) => {
     const url = !isEmpty(e.target.value) ? `/guides?q=${e.target.value}` : `/guides`;
-    router.replace(url, undefined, { shallow: true });
+    void router.replace(url, undefined, { shallow: true });
   }, 300);
+
+  useEffect(() => {
+    const el = inputRef.current;
+    if (el) {
+      el.value = q;
+    }
+  }, [ready, q]);
 
   return (
     <Layout title="Guides" description="Genshin Impact characters' playstyle, builds and etc.">
       <Container>
-        <RouterReady>
-          <input
-            type="text"
-            placeholder="Search by title"
-            onChange={handleChange}
-            className="w-full mb-4 lg:mb-8 leading-6 dark:text-neutral-300 placeholder:text-neutral-600 accent-primary-500 rounded-md ring-1 bg-white dark:bg-neutral-800 ring-neutral-900/10 dark:ring-neutral-50/10 shadow-sm py-1.5 pl-2 pr-3"
-          />
+        {ready && (
+          <>
+            <input
+              ref={inputRef}
+              type="text"
+              placeholder="Search by title"
+              onChange={handleChange}
+              className="w-full mb-4 lg:mb-8 leading-6 dark:text-neutral-300 placeholder:text-neutral-600 accent-primary-500 rounded-md ring-1 bg-white dark:bg-neutral-800 ring-neutral-900/10 dark:ring-neutral-50/10 shadow-sm py-1.5 pl-2 pr-3"
+            />
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
-            {publishedCharacters
-              .filter((c) => c.id.includes(q.toString().toLowerCase()))
-              .map((character) => (
-                <GuideCard
-                  key={character.id}
-                  id={character.id}
-                  href={`/guides/${character.id}`}
-                  title={character.name}
-                  description={character.description}
-                  thumbnail={`/img/characters/${character.id}/avatar_header.webp`}
-                />
-              ))}
-          </div>
-        </RouterReady>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+              {publishedCharacters
+                .filter((c) => c.id.includes(q.toString().toLowerCase()))
+                .map((character) => (
+                  <GuideCard
+                    key={character.id}
+                    id={character.id}
+                    href={`/guides/${character.id}`}
+                    title={character.name}
+                    description={character.description}
+                    thumbnail={`/img/characters/${character.id}/avatar_header.webp`}
+                  />
+                ))}
+            </div>
+          </>
+        )}
       </Container>
     </Layout>
   );
