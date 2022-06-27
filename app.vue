aeac01dfd4ff2963f05ccb651c379f0d158a4c0c
BinaryKitsune <103903444+BinaryKitsune@users.noreply.github.com>
2022-06-27 16:57:37 +0400

    layout causes hydration mismatch for some reason

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <Body class="antialiased bg-neutral-50 dark:bg-neutral-900 dark:text-neutral-100">
+    <div class="app-container">
+      <Navigation />
+      <NuxtPage />
+      <Footer />
+    </div>
+  </Body>
 </template>
 
 <script setup lang="ts">
