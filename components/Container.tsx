beb20953d48824e0bf60f2b1b36425b995f9e2f4
BinaryKitsune <103903444+BinaryKitsune@users.noreply.github.com>
2022-06-28 21:32:56 +0400

    feat(container): removed lg padding

--- components/Container.tsx
+++ components/Container.tsx
@@ -6,10 +6,10 @@ interface Props {
 }
 
 export const Container = ({ verticalCenter = false, children }: Props) => {
-  const verticalClass = verticalCenter ? "place-content-center" : "";
+  const verticalClass = verticalCenter ? `place-content-center` : ``;
 
   return (
-    <main className={"main p-4 lg:p-6 grid " + verticalClass}>
+    <main className={`main p-4 grid ` + verticalClass}>
       <div id="content">{children}</div>
     </main>
   );
