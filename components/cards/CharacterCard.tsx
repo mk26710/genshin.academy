34c4a8b88d2d4c1747f4752173b30ee368f33d75
BinaryKitsune <103903444+BinaryKitsune@users.noreply.github.com>
2022-06-28 19:07:38 +0400

    fix(styles): long-title class is now restored

--- components/cards/CharacterCard.tsx
+++ components/cards/CharacterCard.tsx
@@ -14,25 +14,25 @@ export const CharacterCard: FC<Props> = ({ id, name, rarity }) => {
 
   const iconBg = () => {
     if (rarity === Rarity.FIVE_STAR) {
-      return "from-[#945C2C] to-[#B27330]";
+      return `from-[#945C2C] to-[#B27330]`;
     } else if (rarity === Rarity.FOUR_STAR) {
-      return "from-[#5E5789] to-[#9C75B7]";
+      return `from-[#5E5789] to-[#9C75B7]`;
     }
-    return "from-[#6A6D74] to-[#868586]";
+    return `from-[#6A6D74] to-[#868586]`;
   };
 
-  const fontSize = /\s+/.test(name) && name.length > 10 ? "long-title" : "";
+  const fontSize = /\s+/.test(name) && name.length > 10 ? `card-long-title` : ``;
 
   return (
     <>
       <NextLink href={`/characters/${id}`}>
         <a className="card card-vertical w-[calc(33.33%-0.75rem)] md:w-28">
-          <div className={"w-full aspect-square rounded-t-lg bg-gradient-to-b " + iconBg()}>
+          <div className={`w-full aspect-square rounded-t-lg bg-gradient-to-b ` + iconBg()}>
             <img className="card-thumbnail" src={iconSrc} alt={`${name} icon`} />
           </div>
 
-          <div className="w-full h-8 flex items-center justify-center font-semibold">
-            <p className={"p-1 text-[.9rem] text-center " + fontSize}>{name}</p>
+          <div className="w-full h-9 flex items-center justify-center font-semibold">
+            <p className={`p-1 text-[.9rem] text-center ` + fontSize}>{name}</p>
           </div>
         </a>
       </NextLink>
