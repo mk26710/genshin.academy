9b76fff00ac77e43c2db0372df08a9b4be184a4e
BinaryKitsune <103903444+BinaryKitsune@users.noreply.github.com>
2022-06-29 14:24:22 +0400

    feat(characters, card): element on top left

--- components/cards/CharacterCard.tsx
+++ components/cards/CharacterCard.tsx
@@ -1,3 +1,4 @@
+import type { CharacterType } from "@/data/character";
 import type { FC } from "react";
 
 import NextLink from "next/link";
@@ -5,35 +6,42 @@ import NextLink from "next/link";
 import { Rarity } from "@/data/types/genshin";
 
 interface Props {
-  id: string;
-  name: string;
-  rarity: Rarity;
+  character: CharacterType;
 }
 
-export const CharacterCard: FC<Props> = ({ id, name, rarity }) => {
-  const iconSrc = `/img/characters/${id}/icon.png`;
+export const CharacterCard: FC<Props> = ({ character }) => {
+  const elementSrc = `/img/elements/${character.vision.toLowerCase()}/icon.webp`;
+  const iconSrc = `/img/characters/${character.id}/icon.png`;
+
+  const fontSize =
+    /\s+/.test(character.name) && character.name.length > 10 ? `card-long-title` : ``;
 
   const iconBg = () => {
-    if (rarity === Rarity.FIVE_STAR) {
+    if (character.rarity === Rarity.FIVE_STAR) {
       return `from-[#945C2C] to-[#B27330]`;
-    } else if (rarity === Rarity.FOUR_STAR) {
+    } else if (character.rarity === Rarity.FOUR_STAR) {
       return `from-[#5E5789] to-[#9C75B7]`;
     }
     return `from-[#6A6D74] to-[#868586]`;
   };
 
-  const fontSize = /\s+/.test(name) && name.length > 10 ? `card-long-title` : ``;
-
   return (
     <>
-      <NextLink href={`/characters/${id}`}>
+      <NextLink href={`/characters/${character.id}`}>
         <a className="card card-vertical w-[calc(33.33%-0.75rem)] md:w-28">
+          <div className="bg-white dark:bg-neutral-800 box-border border border-neutral-200 dark:border-neutral-200/10 absolute -ml-2 -mt-2 rounded-full aspect-square w-8 flex items-center justify-center">
+            <img
+              src={elementSrc}
+              alt="Element icon"
+              className="w-5 h-5 aspect-square object-contain object-center"
+            />
+          </div>
           <div className={`w-full aspect-square rounded-t-lg bg-gradient-to-b ` + iconBg()}>
-            <img className="card-thumbnail" src={iconSrc} alt={`${name} icon`} />
+            <img className="card-thumbnail" src={iconSrc} alt={`${character.name} icon`} />
           </div>
 
           <div className="w-full h-9 flex items-center justify-center font-semibold">
-            <p className={`p-1 text-[.9rem] text-center ` + fontSize}>{name}</p>
+            <p className={`p-1 text-[.9rem] text-center ` + fontSize}>{character.name}</p>
           </div>
         </a>
       </NextLink>
--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -11,14 +11,9 @@ const CharactersIndex: NextPage = () => {
       <Container>
         <h1 className="font-semibold text-4xl mb-4 mt-6">Characters</h1>
 
-        <div className="flex flex-row flex-wrap gap-2 justify-evenly md:justify-start">
+        <div className="flex flex-row flex-wrap gap-4 justify-evenly md:justify-start">
           {charactersArray.map((character) => (
-            <CharacterCard
-              key={character.id}
-              id={character.id}
-              name={character.name}
-              rarity={character.rarity}
-            />
+            <CharacterCard key={character.id} character={character} />
           ))}
         </div>
       </Container>
