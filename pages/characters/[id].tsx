66fc76b6c802101f0c3e32447c287d6cb963d070
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-02 20:23:49 +0400

    feat(characters[id]): create basic layout

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -41,9 +41,32 @@ const CharactersId = ({ character }: StaticProps) => {
       iconURL={characterIcon(character.id)}
     >
       <Container>
-        <h1 className="font-semibold text-4xl mb-4 mt-6">{character.name}</h1>
-        <p>{character.description}</p>
-        <img src={avatarPath(character.id, `webp`)} alt={`${character.name} gacha image`} />
+        <div className="flex flex-col-reverse lg:flex-row">
+          <div className="flex-1 grow">
+            <h1 id="name" className="font-semibold text-4xl mb-4 mt-6">
+              {character.name}
+            </h1>
+
+            <p id="description">{character.description}</p>
+
+            <h2 id="consteallations" className="font-semibold text-2xl mt-6 mb-4">
+              Consteallations
+            </h2>
+
+            <ol className="marker:text-sky-500 list-decimal pl-4">
+              {character.constellations.map(({ level, title, description }) => (
+                <li key={`const-${level}`} id={`constellation-${level}`} className="[&+li]:mt-2">
+                  <h3 className="font-semibold text-lg">{title}</h3>
+                  <p className="whitespace-pre-line">{description}</p>
+                </li>
+              ))}
+            </ol>
+          </div>
+
+          <div className="lg:max-w-xs xl:max-w-xl">
+            <img src={avatarPath(character.id, `webp`)} alt={`${character.name} gacha image`} />
+          </div>
+        </div>
       </Container>
     </Layout>
   );
