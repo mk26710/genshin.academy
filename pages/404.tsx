628e28633d8dbdf3a6cd60ba4860828141c1a63f
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-04 22:07:08 +0400

    feat(404): better styles for text

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,12 @@ import { Layout } from "@/components/Layout";
 const NotFound: NextPage = () => {
   return (
     <Layout title="404" description="Not found!">
-      <Container verticalCenter>404 NOT FOUND</Container>
+      <Container verticalCenter>
+        <div className="flex flex-row">
+          <h1 className="text-xl font-semibold mr-2 pr-2 border-r align-middle">404</h1>
+          <h2 className="text-xl align-middle">Not fond</h2>
+        </div>
+      </Container>
     </Layout>
   );
 };
