7adafeedb5313ef1ab4fe500c8c09f495f805b7b
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-05 15:58:17 +0400

    refactor: moved everything in `src/` directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,7 +86,7 @@ const myScreens = {
 };
 
 module.exports = {
-  content: [`./pages/**/*.{js,ts,jsx,tsx}`, `./components/**/*.{js,ts,jsx,tsx}`],
+  content: [`./src/pages/**/*.{js,ts,jsx,tsx}`, `./src/components/**/*.{js,ts,jsx,tsx}`],
   darkMode: `class`,
   theme: {
     extend: {
