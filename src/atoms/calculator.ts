21b352973844c0235a2a8a7c40275e4f6291a348
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-06 12:41:50 +0400

    feat(atoms): created atoms for resin calculations

--- src/atoms/calculator.ts
+++ src/atoms/calculator.ts
@@ -7,3 +7,11 @@ export const critRateAtom = atom<number>(critRateInit);
 
 export const critDamageInit = NaN;
 export const critDamageAtom = atom<number>(critDamageInit);
+
+/** Resin calculator atoms */
+
+export const resinCurrentInit = NaN;
+export const resinCurrentAtom = atom<number>(resinCurrentInit);
+
+export const resinNeededInit = NaN;
+export const resinNeededAtom = atom<number>(resinCurrentInit);
