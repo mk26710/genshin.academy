766cc4d5ce95e8c0b76c89b0db627be2237529e9
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-06 22:51:43 +0400

    feat(scripts): updated webp generator so it makes smaller variants for mobile and uses custom quality for smaller file sizes

--- generatePublicWebpImages.mjs
+++ generatePublicWebpImages.mjs
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const program = new Command();
-program.option(`-r`, `--regenerate`, false).parse(process.argv);
+program.option(`-r`, `--regenerate`, false).option(`-o`, `--optimize`, false).parse(process.argv);
 
 const pattern = join(__dirname, `./public`, `**/*.{png,jpg,jpeg}`);
 const paths = await globby(pattern);
@@ -32,15 +32,38 @@ for (const p of paths) {
 
   if ((await fileExists(webpFilePath)) && program.opts().r !== true) {
     console.log(`* Skipping ${webpFileName} since it already exists at ${webpFilePath}`);
-    continue;
+  } else {
+    console.log(`* generating ${webpFilePath}...`);
+
+    try {
+      await sharp(p).webp({ quality: 90 }).toFile(webpFilePath);
+    } catch (e) {
+      console.error(`SKIP - ERROR WHILE PROCESSING ${p} - ${e}`);
+    }
   }
 
-  console.log(`* generating ${webpFilePath}...`);
+  if (program.opts().o === true) {
+    const webp1200FileName = basename(p, extname(p)) + `_1200.webp`;
+    const webp1200FilePath = join(dirname(p), webp1200FileName);
 
-  try {
-    await sharp(p).webp({ lossless: true }).toFile(webpFilePath);
-  } catch (e) {
-    console.error(`SKIP - ERROR WHILE PROCESSING ${p} - ${e}`);
+    if ((await fileExists(webp1200FilePath)) && program.opts().r !== true) {
+      console.log(`* Skipping ${webp1200FileName} since it already exists at ${webp1200FilePath}`);
+    } else {
+      console.log(`* generating ${webp1200FilePath}...`);
+
+      let newHeight = 1200;
+
+      const metadata = await sharp(p).metadata();
+      if (metadata.height != null && metadata.height < newHeight) {
+        newHeight = metadata.height;
+      }
+
+      try {
+        await sharp(p).resize({ height: newHeight }).webp({ quality: 75 }).toFile(webp1200FilePath);
+      } catch (e) {
+        console.error(`SKIP - ERROR WHILE PROCESSING ${p} - ${e}`);
+      }
+    }
   }
 }
 
