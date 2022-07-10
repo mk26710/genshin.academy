1c13502e9ed6a89b123d31c32dbced77d4a4253c
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-10 22:27:37 +0400

    refactor(pages): updated guides by using atoms to keep state in sync with router

--- src/pages/guides/index.tsx
+++ src/pages/guides/index.tsx
@@ -1,10 +1,14 @@
 import type { CharacterType } from "@/data/character";
 import type { NextPage } from "next";
-import type { ChangeEvent } from "react";
+import type { NextRouter } from "next/router";
+import type { ChangeEvent, FunctionComponent } from "react";
 
-import { debounce } from "lodash-es";
-import { useEffect, useRef } from "react";
+import { useAtom } from "jotai";
+import { useHydrateAtoms } from "jotai/utils";
+import { debounce, isEmpty } from "lodash-es";
+import { useRef, useEffect } from "react";
 
+import { guideSearchQueryAtom, guideSearchTypeAtom } from "@/atoms/guideSearch";
 import { GuideCard } from "@/components/cards/GuideCard";
 import { Container } from "@/components/Container";
 import { Input } from "@/components/Input";
@@ -24,85 +28,98 @@ const publishedGuides = published.reduce((acc, item) => {
   return acc;
 }, new Array<{ meta: PublishedItem; character: CharacterType }>());
 
-const GuidesIndex: NextPage = () => {
-  const [ready, router] = useRouterReady();
+const RouterReadyContent: FunctionComponent<{ router: NextRouter }> = ({ router }) => {
+  // just realized jotai's hydration can be
+  // abused to avoid the useEffects mess
+  // and keep atoms in sync with router :o
+  useHydrateAtoms([
+    [guideSearchQueryAtom, router.query.q ?? ""],
+    [guideSearchTypeAtom, router.query.t ?? "character"],
+  ]);
 
   const inputRef = useRef<HTMLInputElement>(null);
-  const q = router.query.q?.toString() ?? "";
 
-  const guideType = router.query.t?.toString() ?? "character";
+  const [input, setInput] = useAtom(guideSearchQueryAtom);
+  const [guideType, setGuideType] = useAtom(guideSearchTypeAtom);
 
   const handleInputChange = debounce((e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  }, 200);
+
+  const handleOptionChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setGuideType(e.target.value);
+  };
+
+  useEffect(() => {
     void router.replace(
       {
         query: {
           ...router.query,
-          q: e.target.value,
+          q: input,
+          t: guideType,
         },
       },
       undefined,
       { shallow: true },
     );
-  }, 300);
+  }, [input, guideType]);
 
-  const handleOptionChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    void router.replace(
-      {
-        query: { ...router.query, t: e.target.value },
-      },
-      undefined,
-      { shallow: true },
-    );
-  };
-
+  // we need to set input field value if
+  // there's predefined input atom value
   useEffect(() => {
     const el = inputRef.current;
-    if (el) {
-      el.value = q;
+    if (el != null) {
+      if (isEmpty(el.value) && !isEmpty(input)) {
+        el.value = input;
+      }
     }
-  }, [ready, q]);
+  }, [input]);
+
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row mb-4 gap-2">
+        <select
+          value={guideType}
+          onChange={handleOptionChange}
+          className="w-full lg:w-40 form-select appearance-none block h-10 leading-6 dark:text-dark-300 placeholder:text-neutral-400 dark:placeholder:text-dark-400 accent-primary-500 rounded-lg border border-neutral-200 dark:border-dark-200/10 dark:bg-dark-800 bg-whiteshadow-sm py-1.5 pl-2 pr-3"
+        >
+          <option value="character">Characters</option>
+          <option value="general">General</option>
+        </select>
+
+        <Input
+          ref={inputRef}
+          onChange={handleInputChange}
+          placeholder="Search by title"
+          fullWidth
+        />
+      </div>
+
+      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 2xl:columns-5 gap-4 space-y-4">
+        {publishedGuides
+          .filter((g) => g.meta.type === guideType)
+          .filter((g) => g.character.name.toLowerCase().includes(input.toString().toLowerCase()))
+          .map((guide) => (
+            <GuideCard
+              key={guide.character.id}
+              id={guide.character.id}
+              href={`/guides/${guide.meta.id}`}
+              title={guide.character.name}
+              description={guide.character.description}
+              thumbnail={`/img/characters/${guide.character.id}/avatar_header.webp`}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
+
+const GuidesIndex: NextPage = () => {
+  const [ready, router] = useRouterReady();
 
   return (
     <Layout title="Guides" description="Genshin Impact characters' playstyle, builds and etc.">
-      <Container>
-        {ready && (
-          <>
-            <div className="flex flex-col lg:flex-row mb-4 gap-2">
-              <select
-                value={guideType}
-                onChange={handleOptionChange}
-                className="w-full lg:w-40 form-select appearance-none block h-10 leading-6 dark:text-dark-300 placeholder:text-neutral-400 dark:placeholder:text-dark-400 accent-primary-500 rounded-lg border border-neutral-200 dark:border-dark-200/10 dark:bg-dark-800 bg-whiteshadow-sm py-1.5 pl-2 pr-3"
-              >
-                <option value="character">Characters</option>
-                <option value="general">General</option>
-              </select>
-
-              <Input
-                ref={inputRef}
-                placeholder="Search by title"
-                onChange={handleInputChange}
-                fullWidth
-              />
-            </div>
-
-            <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 2xl:columns-5 gap-4 space-y-4">
-              {publishedGuides
-                .filter((g) => g.meta.type === guideType)
-                .filter((g) => g.character.name.toLowerCase().includes(q.toString().toLowerCase()))
-                .map((guide) => (
-                  <GuideCard
-                    key={guide.character.id}
-                    id={guide.character.id}
-                    href={`/guides/${guide.meta.id}`}
-                    title={guide.character.name}
-                    description={guide.character.description}
-                    thumbnail={`/img/characters/${guide.character.id}/avatar_header.webp`}
-                  />
-                ))}
-            </div>
-          </>
-        )}
-      </Container>
+      <Container>{ready && <RouterReadyContent router={router} />}</Container>
     </Layout>
   );
 };
