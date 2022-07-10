a737addc93897a0e54dc3c2c54d3cd42af49907e
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-10 18:40:45 +0400

    refactor(pages): using jotai atom to store user search input on characters list page

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from "next";
 import type { ChangeEvent } from "react";
 
-import { debounce } from "lodash-es";
-import { useState } from "react";
+import { useAtom } from "jotai";
 
+import { characterSearchAtom } from "@/atoms/characterSearch";
 import { CharacterCard } from "@/components/cards/CharacterCard";
 import { Container } from "@/components/Container";
 import { Input } from "@/components/Input";
@@ -11,30 +11,30 @@ import { Layout } from "@/components/Layout";
 import { charactersArray } from "@/data/characters";
 
 const CharactersIndex: NextPage = () => {
-  const [input, setInput] = useState("");
+  const [search, setSearch] = useAtom(characterSearchAtom);
 
-  const deboucnedHandleChange = debounce((e: ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  }, 300);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <Layout title="Characters" description="Genshin Impact playable characters.">
       <Container>
         <div className="mb-4 mt-6 flex flex-col lg:flex-row gap-4">
           <h1 className="font-semibold text-4xl">Characters</h1>
-          <Input
-            placeholder="Search by name"
-            onChange={deboucnedHandleChange}
-            className="translate-y-1"
-          />
+          <Input placeholder="Search by name" onChange={handleChange} value={search} />
         </div>
 
         <div className="flex flex-row flex-wrap gap-4 justify-evenly md:justify-start">
-          {charactersArray
-            .filter((character) => character.name.toLowerCase().includes(input.toLowerCase()))
-            .map((character) => (
-              <CharacterCard key={character.id} character={character} />
-            ))}
+          {charactersArray.map((character) => (
+            <CharacterCard
+              className={
+                character.name.toLowerCase().includes(search.toLowerCase()) ? "" : "hidden"
+              }
+              key={character.id}
+              character={character}
+            />
+          ))}
         </div>
       </Container>
     </Layout>
