a0da89457d9634428b111346234073c3f1ee8cb8
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-10 17:18:16 +0400

    feat(pages): updated the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 
+import Link from "next/link";
+
 import { Container } from "@/components/Container";
 import { Layout } from "@/components/Layout";
 
@@ -7,9 +9,28 @@ const NotFound: NextPage = () => {
   return (
     <Layout title="404" description="Not found!">
       <Container verticalCenter>
-        <div className="flex flex-row">
-          <h1 className="text-xl font-semibold mr-2 pr-2 border-r align-middle">404</h1>
-          <h2 className="text-xl align-middle">Not fond</h2>
+        <div className="flex flex-row gap-6">
+          <div className="pr-6 border-r border-neutral-200 dark:border-neutral-200/10">
+            <h1 className="font-bold text-5xl text-primary-500">404</h1>
+          </div>
+
+          <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-2">
+              <h1 className="font-bold text-5xl -ml-1">Page not found</h1>
+              <h3 className="opacity-60">Please check the URL in the address bar and try again.</h3>
+            </div>
+
+            <div className="flex flex-row gap-4">
+              <Link href="/">
+                <a
+                  role="button"
+                  className="px-4 py-2 rounded-lg bg-primary-500 font-medium text-white"
+                >
+                  Go back home
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
       </Container>
     </Layout>
