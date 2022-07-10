53cb8d94cbf25f155f9249f6aa7891f0615bc6ba
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-10 22:23:46 +0400

    feat(atoms): added atoms for guides list page

---
+++ src/atoms/guideSearch.ts
@@ -0,0 +1,7 @@
+import { atom } from "jotai";
+
+export const guideSearchQueryInit = "";
+export const guideSearchQueryAtom = atom(guideSearchQueryInit);
+
+export const guideSearchTypeInit = "character";
+export const guideSearchTypeAtom = atom(guideSearchQueryInit);
