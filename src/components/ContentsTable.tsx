836963c2931c4adc7ce4281d49a8bfc599f75f3b
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-11 15:15:04 +0400

    feat(ContentsTable): used cards base class and explicitly set text colors

--- src/components/ContentsTable.tsx
+++ src/components/ContentsTable.tsx
@@ -8,10 +8,10 @@ interface Props {
 export const ContentsTable: FC<Props> = ({ title, headings }) => {
   return (
     <div className="hidden lg:flex flex-col min-h-screen max-h-screen">
-      <div className="sticky top-4 overflow-y-auto ml-4 w-64 rounded-lg border border-neutral-200 dark:border-dark-200/10 dark:bg-dark-800 bg-white">
+      <div className="sticky top-4 overflow-y-auto ml-4 w-64 card">
         <div className="flex flex-col gap-y-2 p-4 w-full">
           {title && (
-            <h1 className="pb-2 font-semibold border-b border-neutral-200 dark:border-dark-200/10">
+            <h1 className="pb-2 font-semibold border-b border-neutral-200 dark:border-dark-200/10 text-[#000] dark:text-dark-300">
               {title}
             </h1>
           )}
