d89385ec94520fde3cf0c3c95d5c3fcb981ad800
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-12 15:41:42 +0400

    refactor(tailwind): tw-elements was removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -114,7 +114,6 @@ module.exports = {
     },
   },
   plugins: [
-    require("tw-elements/dist/plugin"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms")({
       strategy: "class",
