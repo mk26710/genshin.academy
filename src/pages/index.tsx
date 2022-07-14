c8a0e43d67aace8f3adaca46b7d6b1c51c261488
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-14 20:55:46 +0400

    fix: weird hydration error that is happening on home page could be caused by birthdays

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 
 import { BirthdayToday } from "@/components/BirthdaysToday";
+import { ClientOnly } from "@/components/ClientOnly";
 import { Container } from "@/components/Container";
 import { Layout } from "@/components/Layout";
 
@@ -12,7 +13,9 @@ const Home: NextPage = () => {
     >
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-          <BirthdayToday />
+          <ClientOnly>
+            <BirthdayToday />
+          </ClientOnly>
         </div>
       </Container>
     </Layout>
