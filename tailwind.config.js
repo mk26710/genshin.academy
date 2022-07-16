44c7b67573a7108b394e5cbb05d5e9f4765c18c8
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-16 17:52:20 +0400

    chore(tailwind): removed redundant entry in content

--- tailwind.config.js
+++ tailwind.config.js
@@ -92,11 +92,7 @@ const myScreens = {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
