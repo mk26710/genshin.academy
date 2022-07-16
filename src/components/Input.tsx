6cb1e27b1d0779b696e269bd3fedd97643c8e48b
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-16 18:47:33 +0400

    feat(components): removed shadow from input fields

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,7 +14,7 @@ export const Input = forwardRef<HTMLInputElement, Props>(
       <input
         ref={ref}
         type={type}
-        className={`h-10 form-input text-[#000] dark:text-dark-300 rounded-lg border border-neutral-200 dark:border-dark-800 bg-white dark:bg-dark-900 placeholder:text-neutral-400 dark:placeholder:text-dark-500 shadow-lg focus:ring-2 focus:ring-primary-500 ${fullWidthClass} ${className}`}
+        className={`h-10 form-input text-[#000] dark:text-dark-300 rounded-lg border border-neutral-200 dark:border-dark-800 bg-white dark:bg-dark-900 placeholder:text-neutral-400 dark:placeholder:text-dark-500 focus:ring-2 focus:ring-primary-500 ${fullWidthClass} ${className}`}
         {...props}
       />
     );
