bc271ef66af028f7dcd37bfb958e68fd82668348
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-23 18:39:26 +0400

    feat(pages): separated each calculator in subcomponents and used derived atoms instead of react state

--- src/pages/calc.tsx
+++ src/pages/calc.tsx
@@ -1,12 +1,16 @@
-import dayjs from "dayjs";
+import type { FunctionComponent } from "react";
+
 import { useAtom } from "jotai";
 import useTranslation from "next-translate/useTranslation";
 
 import {
   critDamageAtom,
   critRateAtom,
+  critValueAtom,
   resinCurrentAtom,
   resinNeededAtom,
+  resinReplenishTimeAtom,
+  resinTimeDeltaAtom,
 } from "@/atoms/calculator";
 import { CalculatorDetails } from "@/components/calculator/CalculatorDetails";
 import { CalculatorInput } from "@/components/calculator/CalculatorInput";
@@ -16,77 +20,86 @@ import { CalculatorTitle } from "@/components/calculator/CalculatorTitle";
 import { Container } from "@/components/Container";
 import { Layout } from "@/components/Layout";
 
-const CalcPage = () => {
-  const { t } = useTranslation();
-
+const CrtitValueCalculator: FunctionComponent = () => {
   const [critRate, setCritRate] = useAtom(critRateAtom);
   const [critDmg, setCritDmg] = useAtom(critDamageAtom);
-  const critValue = critDmg + critRate * 2;
+  const [critValue] = useAtom(critValueAtom);
 
+  return (
+    <CalculatorRoot className="overflow-y-auto break-inside-avoid">
+      <CalculatorTitle>Crit Value</CalculatorTitle>
+      <CalculatorDetails>Provide your stats details</CalculatorDetails>
+      <CalculatorInput
+        value={critRate}
+        setValue={setCritRate}
+        step={0.1}
+        placeholder="Artifact's crit rate"
+      />
+      <CalculatorInput
+        value={critDmg}
+        setValue={setCritDmg}
+        step={0.1}
+        placeholder="Artifact's crit damage"
+        className="mt-2"
+      />
+
+      {critRate > 0 && critDmg > 0 && (
+        <CalculatorResult>
+          Crit Value is{" "}
+          <span className="text-primary-500 dark:text-primary-400 font-semibold">{critValue}</span>
+        </CalculatorResult>
+      )}
+    </CalculatorRoot>
+  );
+};
+
+const ResinCalculator: FunctionComponent = () => {
   const [resinCurrent, setResinCurrent] = useAtom(resinCurrentAtom);
   const [resinNeeded, setResinNeeded] = useAtom(resinNeededAtom);
-  const resinDelta = (resinNeeded - resinCurrent) * 8;
-  const resinReplenishAt = dayjs().add(resinDelta, "minutes");
+  const [resinDelta] = useAtom(resinTimeDeltaAtom);
+  const [resinReplenishTime] = useAtom(resinReplenishTimeAtom);
+
+  return (
+    <CalculatorRoot className="overflow-y-auto break-inside-avoid">
+      <CalculatorTitle>Resin</CalculatorTitle>
+      <CalculatorDetails>Find out when you&apos;ll be able to farm</CalculatorDetails>
+      <CalculatorInput
+        value={resinCurrent}
+        setValue={setResinCurrent}
+        placeholder="How much resin you have"
+      />
+      <CalculatorInput
+        value={resinNeeded}
+        setValue={setResinNeeded}
+        placeholder="How much resin you need"
+        className="mt-2"
+      />
+
+      {resinDelta > 0 && (
+        <CalculatorResult>
+          You will have{" "}
+          <span className="text-primary-500 dark:text-primary-400 font-semibold">
+            {resinNeeded}
+          </span>{" "}
+          at{" "}
+          <span className="text-primary-500 dark:text-primary-400 font-semibold">
+            {resinReplenishTime.format("lll")}
+          </span>
+        </CalculatorResult>
+      )}
+    </CalculatorRoot>
+  );
+};
+
+const CalcPage = () => {
+  const { t } = useTranslation();
 
   return (
     <Layout title={t("common:calculators")} description={t("meta:calculators.description")}>
       <Container>
         <div className="columns-1 md:columns-2 gap-4 space-y-4">
-          <CalculatorRoot className="overflow-y-auto break-inside-avoid">
-            <CalculatorTitle>Crit Value</CalculatorTitle>
-            <CalculatorDetails>Provide your stats details</CalculatorDetails>
-            <CalculatorInput
-              value={critRate}
-              setValue={setCritRate}
-              step={0.1}
-              placeholder="Artifact's crit rate"
-            />
-            <CalculatorInput
-              value={critDmg}
-              setValue={setCritDmg}
-              step={0.1}
-              placeholder="Artifact's crit damage"
-              className="mt-2"
-            />
-
-            {critRate > 0 && critDmg > 0 && (
-              <CalculatorResult>
-                Crit Value is{" "}
-                <span className="text-primary-500 dark:text-primary-400 font-semibold">
-                  {critValue}
-                </span>
-              </CalculatorResult>
-            )}
-          </CalculatorRoot>
-
-          <CalculatorRoot className="overflow-y-auto break-inside-avoid">
-            <CalculatorTitle>Resin</CalculatorTitle>
-            <CalculatorDetails>Find out when you&apos;ll be able to farm</CalculatorDetails>
-            <CalculatorInput
-              value={resinCurrent}
-              setValue={setResinCurrent}
-              placeholder="How much resin you have"
-            />
-            <CalculatorInput
-              value={resinNeeded}
-              setValue={setResinNeeded}
-              placeholder="How much resin you need"
-              className="mt-2"
-            />
-
-            {resinDelta > 0 && (
-              <CalculatorResult>
-                You will have{" "}
-                <span className="text-primary-500 dark:text-primary-400 font-semibold">
-                  {resinNeeded}
-                </span>{" "}
-                at{" "}
-                <span className="text-primary-500 dark:text-primary-400 font-semibold">
-                  {resinReplenishAt.format("lll")}
-                </span>
-              </CalculatorResult>
-            )}
-          </CalculatorRoot>
+          <CrtitValueCalculator />
+          <ResinCalculator />
         </div>
       </Container>
     </Layout>
