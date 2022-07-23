465ff3eb24a28625ff6efb50a5f16ee8627ac67a
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-23 19:34:32 +0400

    feat(pages): updated calculators with i18n translations and changed some styles

--- src/pages/calc.tsx
+++ src/pages/calc.tsx
@@ -14,46 +14,45 @@ import {
 } from "@/atoms/calculator";
 import { CalculatorDetails } from "@/components/calculator/CalculatorDetails";
 import { CalculatorInput } from "@/components/calculator/CalculatorInput";
-import { CalculatorResult } from "@/components/calculator/CalculatorResult";
 import { CalculatorRoot } from "@/components/calculator/CalculatorRoot";
 import { CalculatorTitle } from "@/components/calculator/CalculatorTitle";
 import { Container } from "@/components/Container";
 import { Layout } from "@/components/Layout";
 
 const CrtitValueCalculator: FunctionComponent = () => {
+  const { t } = useTranslation();
+
   const [critRate, setCritRate] = useAtom(critRateAtom);
   const [critDmg, setCritDmg] = useAtom(critDamageAtom);
   const [critValue] = useAtom(critValueAtom);
 
   return (
     <CalculatorRoot className="overflow-y-auto break-inside-avoid">
-      <CalculatorTitle>Crit Value</CalculatorTitle>
-      <CalculatorDetails>Provide your stats details</CalculatorDetails>
+      <CalculatorTitle>
+        <span className="flex-1">{t`calc:crit-value`}</span>
+        {critValue > 0 && <span className="text-primary-500">{critValue.toFixed(2)}</span>}
+      </CalculatorTitle>
+      <CalculatorDetails>{t`calc:cv-details`}</CalculatorDetails>
       <CalculatorInput
         value={critRate}
         setValue={setCritRate}
         step={0.1}
-        placeholder="Artifact's crit rate"
+        placeholder={t`calc:cv-cr-placeholder`}
       />
       <CalculatorInput
         value={critDmg}
         setValue={setCritDmg}
         step={0.1}
-        placeholder="Artifact's crit damage"
+        placeholder={t`calc:cv-cd-placeholder`}
         className="mt-2"
       />
-
-      {critRate > 0 && critDmg > 0 && (
-        <CalculatorResult>
-          Crit Value is{" "}
-          <span className="text-primary-500 dark:text-primary-400 font-semibold">{critValue}</span>
-        </CalculatorResult>
-      )}
     </CalculatorRoot>
   );
 };
 
 const ResinCalculator: FunctionComponent = () => {
+  const { t } = useTranslation();
+
   const [resinCurrent, setResinCurrent] = useAtom(resinCurrentAtom);
   const [resinNeeded, setResinNeeded] = useAtom(resinNeededAtom);
   const [resinDelta] = useAtom(resinTimeDeltaAtom);
@@ -61,32 +60,24 @@ const ResinCalculator: FunctionComponent = () => {
 
   return (
     <CalculatorRoot className="overflow-y-auto break-inside-avoid">
-      <CalculatorTitle>Resin</CalculatorTitle>
-      <CalculatorDetails>Find out when you&apos;ll be able to farm</CalculatorDetails>
+      <CalculatorTitle>
+        <span className="flex-1">{t`calc:resin`}</span>
+        {resinDelta > 0 && (
+          <span className="text-primary-500 normal-case">{resinReplenishTime.format("lll")}</span>
+        )}
+      </CalculatorTitle>
+      <CalculatorDetails>{t`calc:resin-details`}</CalculatorDetails>
       <CalculatorInput
         value={resinCurrent}
         setValue={setResinCurrent}
-        placeholder="How much resin you have"
+        placeholder={t`calc:resin-current-placeholder`}
       />
       <CalculatorInput
         value={resinNeeded}
         setValue={setResinNeeded}
-        placeholder="How much resin you need"
+        placeholder={t`calc:resin-needed-placeholder`}
         className="mt-2"
       />
-
-      {resinDelta > 0 && (
-        <CalculatorResult>
-          You will have{" "}
-          <span className="text-primary-500 dark:text-primary-400 font-semibold">
-            {resinNeeded}
-          </span>{" "}
-          at{" "}
-          <span className="text-primary-500 dark:text-primary-400 font-semibold">
-            {resinReplenishTime.format("lll")}
-          </span>
-        </CalculatorResult>
-      )}
     </CalculatorRoot>
   );
 };
