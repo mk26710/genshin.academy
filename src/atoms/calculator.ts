a11f1792c0d172d6151f1ff8f38bd2899ff122f9
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-23 18:38:43 +0400

    feat(atoms): added derived atoms with results for calculators

--- src/atoms/calculator.ts
+++ src/atoms/calculator.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { atom } from "jotai";
 
 /** Crit value calculator atoms */
@@ -8,6 +9,8 @@ export const critRateAtom = atom<number>(critRateInit);
 export const critDamageInit = NaN;
 export const critDamageAtom = atom<number>(critDamageInit);
 
+export const critValueAtom = atom((get) => get(critDamageAtom) + get(critRateAtom) * 2);
+
 /** Resin calculator atoms */
 
 export const resinCurrentInit = NaN;
@@ -15,3 +18,8 @@ export const resinCurrentAtom = atom<number>(resinCurrentInit);
 
 export const resinNeededInit = NaN;
 export const resinNeededAtom = atom<number>(resinCurrentInit);
+
+export const resinTimeDeltaAtom = atom((get) => (get(resinNeededAtom) - get(resinCurrentAtom)) * 8);
+export const resinReplenishTimeAtom = atom((get) =>
+  dayjs().add(get(resinTimeDeltaAtom), "minutes"),
+);
