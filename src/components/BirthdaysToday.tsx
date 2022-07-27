16161c1e6b454c8e14203290ec81c623ea41a9dc
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-07-27 17:05:05 +0400

    feat(components): Birthdays now support multiple birthdays per day
    Closes #13

--- src/components/BirthdaysToday.tsx
+++ src/components/BirthdaysToday.tsx
@@ -3,13 +3,14 @@ import type { FunctionComponent } from "react";
 import dayjs from "dayjs";
 import Link from "next/link";
 
+import { StaticPicture } from "@/components/StaticPicture";
 import { charactersArray } from "@/data/characters";
 
 const BirthdaysToday: FunctionComponent = () => {
   const now = dayjs();
   const [day, month] = [now.date(), now.month() + 1];
 
-  const celebrant = charactersArray.find((character) => {
+  const celebrants = charactersArray.filter((character) => {
     const [birthdayDay, birthdayMonth] = character.birthday;
     return birthdayDay === day && birthdayMonth === month;
   });
@@ -17,33 +18,26 @@ const BirthdaysToday: FunctionComponent = () => {
   return (
     <div className="box-border flex flex-col rounded-lg border border-neutral-200 bg-white p-4 dark:border-dark-800 dark:bg-dark-900">
       <h1 className="mb-4 text-xl font-semibold text-[#000] dark:text-dark-300">
-        {celebrant == null && <>Characters Birthdays</>}
-        {celebrant != null && <>Happy Birthday 🎉</>}
+        {celebrants.length > 0 ? "🎉 Happy Birthday!" : "Character Birthdays"}
       </h1>
 
-      {celebrant == null && <>There are no birthdays today :&#40;</>}
+      {celebrants.length <= 0 && <>There are no birthdays today :&#40;</>}
 
-      {celebrant != null && (
+      {celebrants.length > 0 && (
         <>
-          <div>
-            <Link key={`bd-${celebrant.id}`} href={`/characters/${celebrant.id}`}>
-              <a className="flex justify-center rounded-lg bg-neutral-100 outline outline-1 outline-neutral-200 dark:bg-dark-900 dark:outline-dark-200/10">
-                <img
-                  className="rounded-lg object-cover object-center"
-                  src={`/img/characters/${celebrant.id}/icon.webp`}
-                  alt={`${celebrant.name} icon`}
-                />
-              </a>
-            </Link>
+          <div className="flex flex-wrap gap-2">
+            {celebrants.map(({ id, name }, index) => (
+              <Link href={`/characters/${id}`} key={index}>
+                <a className="block h-24  w-24 rounded-lg border border-neutral-200 bg-neutral-100 dark:border-dark-800 dark:bg-dark-950">
+                  <StaticPicture
+                    className="rounded-lg"
+                    src={`/img/characters/${id}/icon.webp`}
+                    alt={`${name} Icon`}
+                  />
+                </a>
+              </Link>
+            ))}
           </div>
-
-          <section className="mt-2 text-justify">
-            <p>Today is a special day! It&apos;s a birthday of {celebrant.name}!</p>
-            <p>
-              Make sure to grab your gift and read the letter in game. See what they have prepared
-              for you :&#41;
-            </p>
-          </section>
         </>
       )}
     </div>
