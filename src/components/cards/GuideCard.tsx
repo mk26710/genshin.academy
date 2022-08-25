8e9fc7e2264554eb3c7142c1ed6acb6a6c9a61ea
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-08-25 23:23:29 +0400

    feat(components): shadows were causing performance issues so switched guide cards styles to something simpler

--- src/components/cards/GuideCard.tsx
+++ src/components/cards/GuideCard.tsx
@@ -32,21 +32,19 @@ export const GuideCard: FC<Props> = ({
       <NextLink href={href} prefetch={false}>
         <a
           id={id}
-          className={`mt-4 flex flex-col gap-4 rounded-lg bg-white p-6 drop-shadow-lg dark:bg-dark-900 ${className}`}
+          className={`flex flex-col gap-4 rounded-lg border border-neutral-200 bg-white transition-all duration-200 ease-in-out hover:-translate-y-2 dark:border-dark-800 dark:bg-dark-900 ${className}`}
         >
           {thumbnail && (
-            <div id={htmlId + "-header"}>
-              <img
-                src={thumbnail}
-                className="-mt-12 rounded-lg text-transparent drop-shadow-lg"
-                alt="character icon"
-                height="1920px"
-                width="1080px"
-              />
-            </div>
+            <img
+              src={thumbnail}
+              className="rounded-t-lg text-transparent"
+              alt="character icon"
+              height="1920px"
+              width="1080px"
+            />
           )}
 
-          <div id={htmlId + "-body"} className="flex flex-grow flex-col gap-2">
+          <div id={htmlId + "-body"} className="flex flex-grow flex-col gap-2 px-6 ">
             <h4 className="text-xl font-semibold dark:text-dark-200">{title}</h4>
 
             <div className="text-justify text-sm">
@@ -56,7 +54,7 @@ export const GuideCard: FC<Props> = ({
             </div>
           </div>
 
-          <div id={htmlId + "-footer"} className="mt-2 flex flex-row gap-4">
+          <div id={htmlId + "-footer"} className="mt-2 flex flex-row gap-4 px-6 pb-6 ">
             <img
               className="h-10 w-10 rounded-full text-transparent drop-shadow-lg"
               src={`https://github.com/${author}.png?size=128`}
