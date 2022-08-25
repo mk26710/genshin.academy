40e6460c8f85ef6cb9f021dc61511d1426bb401b
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-08-25 23:13:56 +0400

    feat(pages): query params search is no more, only input field is allowed

--- src/pages/guides/index.tsx
+++ src/pages/guides/index.tsx
@@ -1,16 +1,14 @@
 import type { CharacterType } from "@/data/character.schema";
 import type { MetaType } from "@/data/guides/meta.schema";
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
-import type { NextRouter } from "next/router";
 import type { ChangeEvent, FunctionComponent } from "react";
 
 import { useAtom } from "jotai";
 import { useHydrateAtoms } from "jotai/utils";
 import debounce from "lodash.debounce";
-import isEmpty from "lodash.isempty";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useRef, useEffect } from "react";
+import { useMemo, useRef } from "react";
 
 import { guideSearchQueryAtom, guideSearchTypeAtom } from "@/atoms/guideSearch";
 import { GuideCard } from "@/components/cards/GuideCard";
@@ -18,23 +16,16 @@ import { Container } from "@/components/Container";
 import { Input } from "@/components/Input";
 import { Layout } from "@/components/Layout";
 import { getCharacterById } from "@/data/characters";
-import { useRouterReady } from "@/hooks/useRouterReady";
 import { getAllGuides } from "@/lib/markdownTools";
 
 const NoResult: FunctionComponent = () => {
   return <>There&apos;s nothing here :(</>;
 };
 
-const RouterReadyContent: FunctionComponent<{ router: NextRouter } & StaticProps> = ({
-  router,
-  availableGuides,
-}) => {
-  // just realized jotai's hydration can be
-  // abused to avoid the useEffects mess
-  // and keep atoms in sync with router :o
+const GuidesIndex = ({ availableGuides }: InferGetStaticPropsType<typeof getStaticProps>) => {
   useHydrateAtoms([
-    [guideSearchQueryAtom, router.query.q ?? ""],
-    [guideSearchTypeAtom, router.query.t ?? "all"],
+    [guideSearchQueryAtom, ""],
+    [guideSearchTypeAtom, "all"],
   ]);
 
   const { t } = useTranslation();
@@ -44,10 +35,14 @@ const RouterReadyContent: FunctionComponent<{ router: NextRouter } & StaticProps
   const [input, setInput] = useAtom(guideSearchQueryAtom);
   const [guideType, setGuideType] = useAtom(guideSearchTypeAtom);
 
-  const filteredGuides = availableGuides
-    .filter((g) => (guideType !== "all" ? g.meta.type === guideType : true))
-    .filter((g) => g.meta.title.toLowerCase().includes(input.toString().toLowerCase()))
-    .sort((a, b) => b.meta.publishedAt - a.meta.publishedAt);
+  const filteredGuides = useMemo(
+    () =>
+      availableGuides
+        .filter((g) => (guideType !== "all" ? g.meta.type === guideType : true))
+        .filter((g) => g.meta.title.toLowerCase().includes(input.toString().toLowerCase()))
+        .sort((a, b) => b.meta.publishedAt - a.meta.publishedAt),
+    [input, guideType],
+  );
 
   const handleInputChange = debounce((e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
@@ -57,79 +52,43 @@ const RouterReadyContent: FunctionComponent<{ router: NextRouter } & StaticProps
     setGuideType(e.target.value);
   };
 
-  useEffect(() => {
-    void router.replace(
-      {
-        query: {
-          ...router.query,
-          q: input,
-          t: guideType,
-        },
-      },
-      undefined,
-      { shallow: true },
-    );
-  }, [input, guideType]);
-
-  // we need to set input field value if
-  // there's predefined input atom value
-  useEffect(() => {
-    const el = inputRef.current;
-    if (el != null) {
-      if (isEmpty(el.value) && !isEmpty(input)) {
-        el.value = input;
-      }
-    }
-  }, [input]);
-
-  return (
-    <>
-      <div className="mb-4 flex flex-col gap-2 lg:flex-row">
-        <select
-          value={guideType}
-          onChange={handleOptionChange}
-          className="form-select block h-10 w-full appearance-none rounded-lg border border-neutral-200 bg-white text-sm font-semibold uppercase text-[#000] placeholder:text-neutral-400 focus:ring-2 focus:ring-primary-500 dark:border-dark-800 dark:bg-dark-900 dark:text-dark-300 dark:placeholder:text-dark-500 lg:w-44"
-        >
-          <option value="all">All</option>
-          <option value="character">Characters</option>
-          <option value="general">General</option>
-        </select>
-
-        <Input
-          ref={inputRef}
-          onChange={handleInputChange}
-          placeholder={t`common:search-by-title`}
-          fullWidth
-        />
-      </div>
-
-      <div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {filteredGuides.length <= 0 && <NoResult />}
-        {filteredGuides.map((guide) => (
-          <GuideCard
-            key={guide.character.id}
-            id={guide.character.id}
-            href={`/guides/${guide.meta.id}`}
-            title={guide.meta.title}
-            description={guide.character.description}
-            publishedAtUnix={guide.meta.publishedAt}
-            author={guide.meta.author}
-            thumbnail={`/img/characters/${guide.character.id}/avatar_header_1200.webp`}
-          />
-        ))}
-      </div>
-    </>
-  );
-};
-
-const GuidesIndex = ({ availableGuides }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [ready, router] = useRouterReady();
-  const { t } = useTranslation();
-
   return (
     <Layout title={t("common:guides")} description={t("meta:guides.home.description")}>
       <Container>
-        {ready && <RouterReadyContent router={router} availableGuides={availableGuides} />}
+        <div className="mb-4 flex flex-col gap-2 lg:flex-row">
+          <select
+            value={guideType}
+            onChange={handleOptionChange}
+            className="form-select block h-10 w-full appearance-none rounded-lg border border-neutral-200 bg-white text-sm font-semibold uppercase text-[#000] placeholder:text-neutral-400 focus:ring-2 focus:ring-primary-500 dark:border-dark-800 dark:bg-dark-900 dark:text-dark-300 dark:placeholder:text-dark-500 lg:w-44"
+          >
+            <option value="all">All</option>
+            <option value="character">Characters</option>
+            <option value="general">General</option>
+          </select>
+
+          <Input
+            ref={inputRef}
+            onChange={handleInputChange}
+            placeholder={t`common:search-by-title`}
+            fullWidth
+          />
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {filteredGuides.length <= 0 && <NoResult />}
+          {filteredGuides.map((guide) => (
+            <GuideCard
+              key={guide.character.id}
+              id={guide.character.id}
+              href={`/guides/${guide.meta.id}`}
+              title={guide.meta.title}
+              description={guide.character.description}
+              publishedAtUnix={guide.meta.publishedAt}
+              author={guide.meta.author}
+              thumbnail={`/img/characters/${guide.character.id}/avatar_header_1200.webp`}
+            />
+          ))}
+        </div>
       </Container>
     </Layout>
   );
