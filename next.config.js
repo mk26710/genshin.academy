6c8bea8317a64fc1671ae70b84ad857e6c55a4ee
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-08 21:44:37 +0400

    chore: updated next config and enable swc minification

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { i18n } = require("./next-i18next.config");
 
-// @ts-check
-
 /**
- * @type {import('next').NextConfig}
- **/
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
 
-const nextConfig = {
+module.exports = defineNextConfig({
   output: "standalone",
   reactStrictMode: true,
-  i18n,
-};
-
-module.exports = nextConfig;
+  swcMinify: true,
+  i18n: i18n,
+});
