ed6b38c91a4f7d488e3f1ae51bed30063989a77c
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-12 01:55:26 +0400

    feat(components): ability to override main classes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,14 +2,15 @@ import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  className?: string;
   verticalCenter?: boolean;
 }
 
-export const Container = ({ verticalCenter = false, children }: Props) => {
+export const Container = ({ verticalCenter = false, className = "", children }: Props) => {
   const verticalClass = verticalCenter ? "place-content-center" : "";
 
   return (
-    <main className={`main my-4 grid ${verticalClass}`}>
+    <main className={`main my-4 grid ${verticalClass} ${className}`}>
       <div id="content">{children}</div>
     </main>
   );
