1673c6b97821e95e4d1900296a8aa1edbb9d75cb
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-12 20:56:52 +0400

    feat: added discord authentication with `next-auth`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import type { Session } from "next-auth";
 import type { AbstractIntlMessages } from "next-intl";
 import type { AppProps } from "next/app";
 
 import { Provider as JotaiProvider } from "jotai";
+import { SessionProvider } from "next-auth/react";
 import { NextIntlProvider } from "next-intl";
 import { ThemeProvider } from "next-themes";
 import { useRouter } from "next/router";
@@ -13,7 +15,10 @@ import { Navigation } from "@/components/Navigation";
 
 import "@/assets/styles/main.css";
 
-const MyApp = ({ Component, pageProps }: AppProps<{ messages?: AbstractIntlMessages }>) => {
+const MyApp = ({
+  Component,
+  pageProps,
+}: AppProps<{ messages?: AbstractIntlMessages; session?: Session }>) => {
   const router = useRouter();
 
   useEffect(() => {
@@ -47,17 +52,19 @@ const MyApp = ({ Component, pageProps }: AppProps<{ messages?: AbstractIntlMessa
   }, []);
 
   return (
-    <NextIntlProvider messages={pageProps.messages}>
-      <JotaiProvider>
-        <ThemeProvider attribute="class">
-          <div className="app-container mx-auto max-w-screen-2xl gap-4 px-2">
-            <Navigation />
-            <Component {...pageProps} />
-            <Footer />
-          </div>
-        </ThemeProvider>
-      </JotaiProvider>
-    </NextIntlProvider>
+    <SessionProvider session={pageProps.session}>
+      <NextIntlProvider messages={pageProps.messages}>
+        <JotaiProvider>
+          <ThemeProvider attribute="class">
+            <div className="app-container mx-auto max-w-screen-2xl gap-4 px-2">
+              <Navigation />
+              <Component {...pageProps} />
+              <Footer />
+            </div>
+          </ThemeProvider>
+        </JotaiProvider>
+      </NextIntlProvider>
+    </SessionProvider>
   );
 };
 
