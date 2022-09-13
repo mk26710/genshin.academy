5c0e03d526094771fc6cab74504c6567d75b8bf2
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-13 18:59:09 +0400

    feat(components): contents table now supports dark mode

--- src/components/ContentsTable.tsx
+++ src/components/ContentsTable.tsx
@@ -10,8 +10,8 @@ export const ContentsTable: FC<Props> = ({ title, headings }) => {
     <div className="hidden flex-col space-y-1 lg:flex">
       <div className="card sticky right-0 top-4 w-60 overflow-y-auto">
         <div className="flex w-full flex-col gap-y-2">
-          <div className="mb-0 self-center border-b border-gray-200 pb-3">
-            <h1 className="font-semibold">{title}</h1>
+          <div className="mb-0 self-center border-b border-gray-200 pb-3 dark:border-neutral-700">
+            <h1 className="font-semibold dark:text-neutral-300">{title}</h1>
           </div>
 
           <div className="flex flex-col gap-y-0">
@@ -19,7 +19,7 @@ export const ContentsTable: FC<Props> = ({ title, headings }) => {
               <a
                 key={`guide-nav-${heading}`}
                 href={`#${heading}`}
-                className="block rounded-lg px-4 py-2 text-sm font-medium capitalize text-gray-700 hover:bg-gray-100"
+                className="block rounded-lg px-4 py-2 text-sm font-medium capitalize text-gray-700 hover:bg-gray-100 dark:text-neutral-400"
               >
                 {heading.replaceAll("-", " ")}
               </a>
