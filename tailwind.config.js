cfb418df88c24a64cabb2e76daccac7ded98d2cd
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-13 11:52:35 +0400

    feat(tailwind): enable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -94,7 +94,8 @@ const myScreens = {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
-  // darkMode: "class",
+  // darkMode: ["class", '[data-colour="dark"]'],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
