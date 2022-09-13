61a5631991276b9ec217ff6eaa9d4180acd565d4
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-13 19:01:09 +0400

    refactor(pages): removed redundant import in 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,6 @@
 import type { GetStaticProps, NextPage } from "next";
 
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 
 import { Container } from "@/components/Container";
 import { Layout } from "@/components/Layout";
