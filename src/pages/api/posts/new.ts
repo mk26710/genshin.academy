9e0be053ad807c1de8ac25a339ac2a40311a89d3
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-20 22:33:01 +0400

    refactor(api): use schema from predefined schemas

--- src/pages/api/posts/new.ts
+++ src/pages/api/posts/new.ts
@@ -1,25 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { PostType, Prisma } from "@prisma/client";
-import { z } from "zod";
+import { Prisma } from "@prisma/client";
 
 import { getServerAuthSession } from "@/server/common/get-server-auth-session";
 import { minimumRoleWriter } from "@/server/common/permissions";
 import { prisma } from "@/server/db/client";
-
-const FormDataValidator = z.object({
-  title: z.string().min(3),
-  description: z.string().min(3),
-  slug: z
-    .string()
-    .min(3, "Slug must have at least 3 characters.")
-    .regex(/^[\w\d]+(?:-[\w\d]+)*$/gi, 'Slug must contain only latin characters and dashes ("-").'),
-  lang: z.union([z.literal("en"), z.literal("ru")]),
-  type: z.custom<PostType>((data) => `${data}` in PostType),
-  thumbnail: z.string().url(),
-  tags: z.string().min(2),
-  text: z.string().min(1, "Text must contain at least one character."),
-});
+import { PostsNewOrEditForm } from "@/server/schemas/posts";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerAuthSession({ req, res });
@@ -33,7 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  const parsedBody = FormDataValidator.safeParse(req.body);
+  const parsedBody = PostsNewOrEditForm.safeParse(req.body);
   if (parsedBody.success !== true) {
     res.status(400).json(parsedBody);
     return;
