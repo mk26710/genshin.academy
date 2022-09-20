78fdc04e393c2d99a086479bc4cfb9db6b25d843
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-20 22:41:54 +0400

    feat(models): added `canUserEditPost` to posts model

--- src/server/db/models/posts.ts
+++ src/server/db/models/posts.ts
@@ -1,4 +1,7 @@
+import type { Post, User } from "@prisma/client";
+
 import { prisma } from "@/server/db/client";
+import { userHasAnyRole } from "@/utils/permissions";
 
 export const getPostBySlug = async (slug: string) => {
   return await prisma.post.findFirst({ where: { slug }, include: { content: true } });
@@ -83,4 +86,22 @@ export const searchPostsPaginated = async ({
       },
     },
   });
+};
+
+type PostLikeObject = Record<string, unknown> & Pick<Post, "authorId">;
+type UserLikeObject = Record<string, unknown> & {
+  id: User["id"];
+  role: User["role"];
+};
+
+export const canUserEditPost = (user: Nil<UserLikeObject>, post: Nil<PostLikeObject>) => {
+  if (user == null || post == null) {
+    return false;
+  }
+
+  if (user.id === post.authorId || userHasAnyRole(user, "ADMIN", "MODERATOR")) {
+    return true;
+  }
+
+  return false;
 };
