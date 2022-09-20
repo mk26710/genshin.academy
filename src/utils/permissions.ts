8bc3065297fe3a1b5d0121e7f0cc3f8265f0d79b
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-09-20 22:32:07 +0400

    feat(utils): updated `userHasAnyRole` so it can accept nullish values

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -1,12 +1,26 @@
 import type { Role, User } from "@prisma/client";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type UserWithRole = Record<any, unknown> & Pick<User, "role">;
+type UserWithRole = Record<any, unknown> & { role: User["role"] | null | undefined };
 
-/** Checks if specfied object's role is inlcuded in provided list of allowed roles */
-export const userHasAnyRole = (userWithRole: UserWithRole, ...allowedRoles: Role[]) => {
+/** Checks if specfied object's role is inlcuded in provided list of allowed roles.
+ *
+ * This will return `false` if a `userWithRole` is a nullish value and also if
+ * `userWithRole#role` is nullish too.
+ *
+ * This will throw a `TypeError` if `userWithRole` doesn't have a `role` property.
+ */
+export const userHasAnyRole = (userWithRole?: UserWithRole, ...allowedRoles: Role[]) => {
+  if (userWithRole == null) {
+    return false;
+  }
+
   if (!("role" in userWithRole)) {
     throw TypeError(`Provided object doesn't have a "role" property.`);
+  }
+
+  if (userWithRole.role == null) {
+    return false;
   }
 
   return allowedRoles.includes(userWithRole.role);
