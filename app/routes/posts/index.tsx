33ac2d6717a38fcb9bf69265a86feb6594367e99
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-06 20:19:32 +0400

    feat(routes): updated psots search route and improved search params validation and added some transformations

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,19 +1,16 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { LoaderArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Response } from "@remix-run/node";
 import { useLoaderData, useSearchParams } from "@remix-run/react";
-import { useEffect, useState } from "react";
+import type { ChangeEvent } from "react";
+import { useEffect } from "react";
+import { PostCard } from "~/components/cards/PostCard";
 import { Container } from "~/components/Container";
-import { searchPostsPaginated } from "~/models/posts.server";
+import { countSearchPostsPaginated, searchPostsPaginated } from "~/models/posts.server";
 import { PostsSearch } from "~/schemas/posts";
 import { resolveLocale } from "~/utils/i18n.server";
 
-type LoaderData = {
-  page: number;
-  posts: Awaited<ReturnType<typeof searchPostsPaginated>>;
-};
-
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader = async ({ request }: LoaderArgs) => {
   const locale = await resolveLocale(request);
 
   const url = new URL(request.url);
@@ -22,6 +19,11 @@ export const loader: LoaderFunction = async ({ request }) => {
   const page = pageParam != null ? parseInt(pageParam.toString()) : 1;
   const postsPerPage = 6;
 
+  const searchLangs = await Promise.resolve(url.searchParams.getAll("lang"))
+    .then((arr) => arr.filter((el) => el.length > 0))
+    .then((arr) => [...new Set(arr)])
+    .then((arr) => (arr.length > 0 ? arr : [locale]));
+
   const parseResult = PostsSearch.safeParse({
     skip: (page - 1) * postsPerPage,
     take: postsPerPage,
@@ -29,17 +31,15 @@ export const loader: LoaderFunction = async ({ request }) => {
     authorName: url.searchParams.get("author") ?? undefined,
     type: url.searchParams.get("type") ?? undefined,
     query: url.searchParams.get("search")?.toString().trim().replaceAll(/\s+/gi, " & "),
-    lang: url.searchParams.get("lang")?.split(",") ?? [locale],
+    lang: searchLangs,
   });
 
   if (parseResult.success !== true) {
-    console.error(parseResult.error);
     throw new Response("Bad Request", { status: 400, statusText: "Bad Request" });
   }
 
   const { skip, take, lang, order, type, authorName, query: searchTitle } = parseResult.data;
-
-  const posts = await searchPostsPaginated({
+  const searchOptions = {
     skip,
     take,
     lang,
@@ -47,27 +47,102 @@ export const loader: LoaderFunction = async ({ request }) => {
     type,
     authorName,
     searchTitle,
-  });
+  };
 
-  return json<LoaderData>({ posts, page });
+  const totalPosts = await countSearchPostsPaginated(searchOptions);
+  const posts = await searchPostsPaginated(searchOptions);
+
+  return json({ posts, totalPosts, page, searchLangs });
+};
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Posts - GENSHIN.ZENLESS",
+  };
 };
 
 const PostsIndexRoute = () => {
-  const { posts, page } = useLoaderData() as LoaderData;
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    console.log(posts);
-  }, [posts]);
+  const { posts, page, searchLangs } = useLoaderData<typeof loader>();
+  const [search, setSearch] = useSearchParams();
 
   const handlePageIncrease = () => {
-    setSearchParams({ page: `${page + 1}` });
+    setSearch({ page: `${page + 1}` });
   };
 
+  const handleLanguageSelect = async (e: ChangeEvent<HTMLInputElement>) => {
+    const langs = new Set(search.getAll("lang"));
+    if (e.target.checked) {
+      if (!langs.has(e.target.value)) {
+        langs.add(e.target.value);
+      }
+    } else {
+      if (langs.has(e.target.value)) {
+        langs.delete(e.target.value);
+      }
+    }
+    console.log("a " + search.getAll("lang"));
+
+    setSearch({ lang: [...langs] });
+  };
+
+  useEffect(() => {
+    console.log(searchLangs);
+  }, []);
+
   return (
-    <Container>
-      hello
-      <button onClick={handlePageIncrease}>+Page {page}</button>
+    <Container className="px-0 pt-0 lg:px-[var(--default-gap)]">
+      <div className="grid h-full grid-rows-[auto_auto] gap-[var(--default-gap)] lg:grid-cols-[1fr_auto] lg:grid-rows-1">
+        <div className="card sticky top-[var(--header-height)] z-[5] flex h-fit w-full flex-col rounded-none border-b border-t-0 bg-white lg:top-[calc(var(--header-height)_+_var(--default-gap))] lg:w-64 lg:rounded-md lg:border-r lg:border-l lg:border-t">
+          <button onClick={handlePageIncrease} className="button">
+            plus page
+          </button>
+
+          <div role="group">
+            <label>
+              <input
+                type="checkbox"
+                name="lang"
+                value="ru"
+                onChange={handleLanguageSelect}
+                checked={searchLangs.includes("ru")}
+              />{" "}
+              ru
+            </label>
+
+            <label>
+              <input
+                type="checkbox"
+                name="lang"
+                value="en"
+                onChange={handleLanguageSelect}
+                checked={searchLangs.includes("en")}
+              />{" "}
+              en
+            </label>
+          </div>
+        </div>
+
+        {posts.length <= 0 && (
+          <div className="flex h-full w-full items-center justify-center lg:row-end-1">
+            Nothing here :(
+          </div>
+        )}
+
+        {posts.length > 0 && (
+          <div className="columns-1 space-y-[var(--default-gap)] px-[var(--default-gap)] lg:row-end-1 lg:columns-2 lg:px-0 lg:pt-[var(--default-gap)]">
+            {posts.map((post) => (
+              <PostCard
+                key={post.id}
+                slug={post.slug}
+                title={post.title}
+                description={post.description}
+                thumbnailUrl={post.thumbnailUrl}
+                publishedAt={new Date(post.publishedAt)}
+              />
+            ))}
+          </div>
+        )}
+      </div>
     </Container>
   );
 };
