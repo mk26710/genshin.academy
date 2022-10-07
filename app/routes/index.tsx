c4913eeb98b928225820bca36fb0ea31eb80ae5e
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-07 21:32:08 +0400

    feat(routes): index page is now going to show website's latest post

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,44 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderArgs, MetaFunction, SerializeFrom } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { PostCard } from "~/components/cards/PostCard";
 import { Container } from "~/components/Container";
+import { getLatestPost } from "~/models/posts.server";
+import { resolveLocale } from "~/utils/i18n.server";
+
+type LoaderData = SerializeFrom<typeof loader>;
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const resolvedLocale = await resolveLocale(request);
+
+  const latestPost = await getLatestPost({ lang: resolvedLocale });
+
+  return json({ latestPost });
+};
 
 export const meta: MetaFunction = () => ({
   title: "Home - GENSHIN.ZENLESS",
 });
 
-export default function Index() {
-  return <Container>Hello</Container>;
-}
+const IndexRoute = () => {
+  const { latestPost } = useLoaderData() as LoaderData;
+
+  return (
+    <Container>
+      <div className="columns-1 md:columns-2 lg:columns-3">
+        {/* test */}
+        {latestPost && (
+          <PostCard
+            slug={latestPost.slug}
+            title={latestPost.title}
+            description={latestPost.description}
+            publishedAt={new Date(latestPost.publishedAt)}
+            thumbnailUrl={latestPost.thumbnailUrl}
+          />
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default IndexRoute;
