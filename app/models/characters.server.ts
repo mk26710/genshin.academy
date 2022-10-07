be9896a3972506d81b7011e0ea5d9d5511bd9315
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-07 22:18:43 +0400

    feat(models): support getting data for multiple languages at once for `getCharactersList()`

--- app/models/characters.server.ts
+++ app/models/characters.server.ts
@@ -1,24 +1,22 @@
 import { prisma } from "~/db/prisma.server";
 
 interface GetCharactersOptions {
-  lang?: string;
+  langs?: string[];
 }
 
 export const getCharactersList = async (opts?: GetCharactersOptions) => {
   return await prisma.genshinCharacter.findMany({
-    where: {
-      identity: {
-        every: {
-          lang: opts?.lang,
-        },
-      },
-    },
     select: {
       id: true,
       rarity: true,
       vision: true,
       weapon: true,
       identity: {
+        where: {
+          lang: {
+            in: opts?.langs,
+          },
+        },
         select: {
           lang: true,
           name: true,
