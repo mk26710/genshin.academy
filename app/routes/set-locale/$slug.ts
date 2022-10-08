59be307d388c2d078c5a111598a769191345897c
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-09 01:13:24 +0400

    feat(rotues): `/set-locale` is now an action only route

--- app/routes/set-locale/$slug.ts
+++ app/routes/set-locale/$slug.ts
@@ -1,16 +1,17 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { safeRedirect } from "~/utils/helpers";
 import { localeCookie, safePickLocale } from "~/utils/i18n.server";
+import type { UserLocale } from "~/utils/locales";
 import { supportedLocales } from "~/utils/locales";
 import { text } from "~/utils/responses.server";
 
-export const loader: LoaderFunction = async ({ request, params }) => {
+export const action: ActionFunction = async ({ request, params }) => {
   if (typeof params.slug !== "string") {
     return text("Slug is not a string", { status: 500 });
   }
 
-  if (!supportedLocales.includes(params.slug)) {
+  if (!supportedLocales.includes(params.slug as UserLocale)) {
     return text(`Unsupported locale was provided`, { status: 400 });
   }
 
