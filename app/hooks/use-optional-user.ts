b27bf5911fc08e8e1e0fc533f8b16af5ccc1117b
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-09 18:41:33 +0400

    feat(hooks): more strict checks on `useOptionalUser` hook and now it'll also look for user data in catch data of a catch boundary

--- app/hooks/use-optional-user.ts
+++ app/hooks/use-optional-user.ts
@@ -1,15 +1,25 @@
-import type { getUser } from "~/utils/session.server";
+import { useCatch } from "@remix-run/react";
+import type { GetUser } from "~/utils/session.server";
 import { useMatchesData } from "./use-matches-data";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function isUser(user: any): user is NonNullable<Awaited<ReturnType<typeof getUser>>> {
+function isUser(user: any): user is NonNullable<GetUser> {
   return user && typeof user === "object" && typeof user.name === "string";
 }
 
-export const useOptionalUser = (): Awaited<ReturnType<typeof getUser>> | undefined => {
+export const useOptionalUser = (): GetUser | undefined => {
   const data = useMatchesData("root");
-  if (!data || !isUser(data.user)) {
-    return undefined;
+
+  const caught = useCatch();
+  const caughtData = caught.data;
+
+  if (data && typeof data === "object" && "user" in data && isUser(data.user)) {
+    return data.user;
   }
-  return data.user;
+
+  if (caught && typeof caughtData === "object" && "user" in caughtData && isUser(caughtData.user)) {
+    return caughtData.user;
+  }
+
+  return undefined;
 };
