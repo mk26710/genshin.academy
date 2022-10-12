540f04b888e540a00c87322035fa7168d7db09ac
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-12 11:30:36 +0400

    feat(eslint): enabled import order plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,13 @@
-/** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+  ],
   env: {
     node: true,
     es6: true,
@@ -16,6 +20,48 @@ module.exports = {
     "@typescript-eslint/restrict-template-expressions": "off",
     "@next/next/no-img-element": "off",
     "react-hooks/exhaustive-deps": "off",
+    "import/no-named-as-default": 0,
+    "import/no-named-as-default-member": 0,
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "type",
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling", "index"],
+          "object",
+          "unknown",
+        ],
+        pathGroups: [
+          {
+            pattern: "**/*.css",
+            group: "unknown",
+            position: "after",
+          },
+          {
+            pattern: "**/*.scss",
+            group: "unknown",
+            position: "after",
+          },
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+    },
   },
   ignorePatterns: ["node_modules/", "build/", "dist/", "out/", ".next/"],
 };
