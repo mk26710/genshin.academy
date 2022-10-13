8ce940f950892def40ae3c23e74d5f8e4a61a48d
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-13 18:36:17 +0400

    feat(routes): list of registered users

---
+++ app/routes/yashiro/users/index.tsx
@@ -0,0 +1,37 @@
+import type { LoaderArgs } from "@remix-run/node";
+
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { UserCard } from "~/components/cards/UserCard";
+import { Container } from "~/components/Container";
+import { prisma } from "~/db/prisma.server";
+import { userHasAnyRole } from "~/utils/permissions";
+import { ensureAuthorizedUser } from "~/utils/session.server";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  await ensureAuthorizedUser(request, async (user) => userHasAnyRole(user, "ADMIN"));
+
+  const registeredUsers = await prisma.user.findMany({
+    include: { roles: true },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return json({ registeredUsers });
+};
+
+const YashiroUsersRoute = () => {
+  const { registeredUsers } = useLoaderData<typeof loader>();
+
+  return (
+    <Container>
+      <div className="flex w-full flex-wrap gap-2">
+        {registeredUsers.map((user) => (
+          <UserCard key={user.id} {...user} to={`./${user.id}`} isLink />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default YashiroUsersRoute;
