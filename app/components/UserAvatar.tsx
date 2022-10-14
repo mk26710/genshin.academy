52277382977dd38118447eb8ca27370ad05e36f0
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-14 16:35:17 +0400

    feat(components): added `realAvatarUrl` to `UserAvatar` which what kind of data was provided in the props

--- app/components/UserAvatar.tsx
+++ app/components/UserAvatar.tsx
@@ -11,9 +11,19 @@ export const UserAvatar = forwardRef<HTMLImageElement, UserAvatarProps>((props, 
   const defaultAvatarUrl =
     "https://cdn.discordapp.com/attachments/1002129706591715379/1029777815287124018/default.webp";
 
+  // if props.avatarUrl is nullish set to default avatar
+  // otherwise checks if props.avatarUrl is less or equeal
+  // to 1 character then sets to default othewise to props.avatarUrl
+  const realAvatarUrl =
+    props.avatarUrl == null
+      ? defaultAvatarUrl
+      : props.avatarUrl.length <= 1
+      ? defaultAvatarUrl
+      : props.avatarUrl;
+
   return (
     <img
-      src={props.avatarUrl ?? defaultAvatarUrl}
+      src={realAvatarUrl}
       ref={ref}
       decoding="async"
       loading="lazy"
