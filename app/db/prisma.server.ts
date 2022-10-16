babb675798d645248b44e1909564cd92c8e64b89
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-16 21:42:55 +0400

    feat(db): format errors with `minimal` on both prod and dev

--- app/db/prisma.server.ts
+++ app/db/prisma.server.ts
@@ -12,11 +12,12 @@ declare global {
 // create a new connection to the DB with every change either.
 // in production we'll have a single connection to the DB.
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({ errorFormat: "minimal" });
   console.log("[PROD] Prisma client created");
 } else {
   if (!global.__db__) {
     global.__db__ = new PrismaClient({
+      errorFormat: "minimal",
       log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
     });
     console.log("[DEV] Prisma client created");
