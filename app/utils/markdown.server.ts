d839aee59fc794a28781a082d894dd65b797e55e
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-21 19:48:00 +0400

    feat(utils): created `extractHeadings`

--- app/utils/markdown.server.ts
+++ app/utils/markdown.server.ts
@@ -6,6 +6,10 @@ import remarkParse from "remark-parse";
 import remark2Rehype from "remark-rehype";
 import { unified } from "unified";
 
+const headingRegex = new RegExp(
+  /\<h(?<level>[1-6])(.+?)id=\"(?<id>[^"]*)\"(.*?)\>(?<value>.*?)<\/h[1-6]>/gm,
+);
+
 export const markdownParser = unified()
   .use(remarkParse)
   .use(remarkGfm)
@@ -14,3 +18,10 @@ export const markdownParser = unified()
   // .use(rehypeSanitize, { tagNames: [...sanitizeDefault.tagNames, "details", "iframe", "summary"] })
   .use(rehypeSlug)
   .use(rehypeStringify);
+
+export const extractHeadings = (content: string) =>
+  Array.from(content.matchAll(headingRegex)).map((match) => ({
+    level: parseInt(match.groups?.level ?? "-1"),
+    id: match.groups?.id,
+    value: match.groups?.value,
+  }));
