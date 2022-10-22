3d05d0a6122be82ecb3d46dd927e197a06131b31
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-22 23:29:12 +0400

    feat(components): added `withScrollRestoration` to `Container` so scroll restoration could be used where needed

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,11 +1,24 @@
 import type { ReactNode } from "react";
 
+import { ScrollRestoration } from "@remix-run/react";
+import { Fragment } from "react";
+
 interface Props {
   children: ReactNode;
   className?: string;
+  withScrollRestoration?: boolean;
   verticalCenter?: boolean;
 }
 
-export const Container = ({ className = "", children }: Props) => {
+export const Container = ({ className = "", withScrollRestoration = false, children }: Props) => {
+  if (withScrollRestoration === true) {
+    return (
+      <Fragment>
+        <ScrollRestoration />
+        <main className={`main ${className}`}>{children}</main>
+      </Fragment>
+    );
+  }
+
   return <main className={`main ${className}`}>{children}</main>;
 };
