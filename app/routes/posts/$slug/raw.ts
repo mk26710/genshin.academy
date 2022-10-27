361ef2595871e657b0706e8552899421e074305e
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-28 00:01:38 +0400

    feat(routes): updated authorization check for raw post

--- app/routes/posts/$slug/raw.ts
+++ app/routes/posts/$slug/raw.ts
@@ -3,15 +3,11 @@ import type { LoaderArgs } from "@remix-run/node";
 import { Response } from "@remix-run/node";
 
 import { getPostBySlugWithAuthor } from "~/models/posts.server";
-import { userHasAnyRole } from "~/utils/permissions";
+import { canUserEditPost } from "~/utils/permissions";
 import { text } from "~/utils/responses.server";
 import { ensureAuthorizedUser } from "~/utils/session.server";
 
 export const loader = async ({ request, params }: LoaderArgs) => {
-  await ensureAuthorizedUser(request, async (user) =>
-    userHasAnyRole(user, "ADMIN", "DEVELOPER", "MODERATOR", "WRITER"),
-  );
-
   const slug = params?.slug;
   if (typeof slug !== "string") {
     throw text("Post slug is not a string somehow", { status: 500 });
@@ -21,6 +17,8 @@ export const loader = async ({ request, params }: LoaderArgs) => {
   if (!post) {
     throw text("Post not found", { status: 404 });
   }
+
+  await ensureAuthorizedUser(request, async (user) => canUserEditPost(user, post));
 
   return new Response(`${post.content?.raw}`);
 };
