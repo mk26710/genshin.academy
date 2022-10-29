794d2b5ffb016c1871106c84dbd55f111f575019
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-10-29 16:50:15 +0400

    feat(utils): a custom rehype plugin that makes all images lazy and async

--- app/utils/markdown.server.ts
+++ app/utils/markdown.server.ts
@@ -1,10 +1,12 @@
 import rehypeRaw from "rehype-raw";
+// import rehypeSanitize, { defaultSchema as sanitizeDefault } from "rehype-sanitize";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
 import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remark2Rehype from "remark-rehype";
 import { unified } from "unified";
+import { visit } from "unist-util-visit";
 
 const headingRegex = new RegExp(
   /\<h(?<level>[1-6])(.+?)id=\"(?<id>[^"]*)\"(.*?)\>(?<value>.*?)<\/h[1-6]>/gm,
@@ -15,8 +17,18 @@ export const markdownParser = unified()
   .use(remarkGfm)
   .use(remark2Rehype, { allowDangerousHtml: true })
   .use(rehypeRaw, { passThrough: ["details", "summary"] })
-  // .use(rehypeSanitize, { tagNames: [...sanitizeDefault.tagNames, "details", "iframe", "summary"] })
   .use(rehypeSlug)
+  // Adds lazy loading and async decoding to img elements
+  .use(() => (root) => {
+    visit(root, "element", (node) => {
+      if (node.tagName === "img") {
+        node.properties = { ...node.properties, decoding: "async", loading: "lazy" };
+      }
+    });
+  })
+  // .use(rehypeSanitize, {
+  //   tagNames: [...(sanitizeDefault.tagNames ?? []), "details", "iframe", "summary"],
+  // })
   .use(rehypeStringify);
 
 export const extractHeadings = (content: string) =>
