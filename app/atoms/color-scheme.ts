1aa39f6fd27fac573db988616bf846516267dc42
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-11-01 21:09:56 +0400

    feat(atoms): added a derived `colorSchemeAtom` with a type guarded setter

--- app/atoms/color-scheme.ts
+++ app/atoms/color-scheme.ts
@@ -2,4 +2,17 @@ import type { ColorScheme } from "~/utils/color-scheme/common";
 
 import { atom } from "jotai";
 
-export const colorSchemeAtom = atom<ColorScheme | null | undefined>(undefined);
+import { isColorScheme } from "~/utils/color-scheme/common";
+
+export const rawColorSchemeAtom = atom<ColorScheme | null | undefined>(undefined);
+
+export const colorSchemeAtom = atom(
+  (get) => get(rawColorSchemeAtom),
+  (_get, set, value: string) => {
+    if (!isColorScheme(value)) {
+      throw TypeError("Invalid color scheme value");
+    }
+
+    set(rawColorSchemeAtom, value);
+  },
+);
