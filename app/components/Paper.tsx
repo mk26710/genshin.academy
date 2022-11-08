0ffc6b8eeef0b3e6ded2ec8c0ebb9dbd72b0a257
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-11-08 22:11:43 +0400

    refactor(components): updated generics for `Paper`

--- app/components/Paper.tsx
+++ app/components/Paper.tsx
@@ -1,39 +1,22 @@
-import type { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from "react";
+import type { ElementType } from "react";
+import type { PolymorphicProps } from "~/types/react";
 
 import clsx from "clsx";
 
-type AsProp<C extends ElementType> = {
-  as?: C;
+type PaperProps = {
+  className?: string;
 };
 
-type PolymorphicPropsWithoutRef<
-  C extends ElementType,
-  P = Record<string, unknown>,
-> = PropsWithChildren<P> & AsProp<C> & ComponentPropsWithoutRef<C>;
-
-type PaperProps<C extends ElementType> = PolymorphicPropsWithoutRef<
-  C,
-  {
-    className?: string;
-  }
->;
-
-export const Paper = <As extends ElementType = "div">({
+export const Paper = <Component extends ElementType = "div">({
   as,
   children,
   className,
   ...rest
-}: PaperProps<As>) => {
+}: PolymorphicProps<PaperProps, Component>) => {
   const Component = as ?? "div";
 
   return (
-    <Component
-      className={clsx(
-        "grid break-inside-avoid grid-flow-row auto-rows-min grid-cols-1 rounded-lg border p-4 shadow-sm transition-all duration-100 ease-in hover:scale-[1.01] hover:shadow-lg dark:border-neutral-700 dark:bg-neutral-800 dark:hover:brightness-125",
-        className,
-      )}
-      {...rest}
-    >
+    <Component className={clsx("paper", className)} {...rest}>
       {children}
     </Component>
   );
