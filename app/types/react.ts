a2f1069d2f64519a2358f45634714bfad3cbcee5
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-11-11 16:01:14 +0400

    feat(types); converted AsProp to interface

--- app/types/react.ts
+++ app/types/react.ts
@@ -1,8 +1,8 @@
 import type { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from "react";
 
-export type AsProp<C extends ElementType> = {
+export interface AsProp<C extends ElementType> {
   as?: C;
-};
+}
 
 export type PolymorphicProps<
   P = Record<string, unknown>,
