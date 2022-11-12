a7569a10a67358bc5bf364e4e5959e15ca39a1c8
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-11-13 01:48:27 +0400

    feat(utils/s3): exported `S3_DOMAIN` and updated uploader for users

--- app/utils/s3.server.ts
+++ app/utils/s3.server.ts
@@ -1,13 +1,11 @@
-import {
-  S3Client,
-  PutObjectCommand,
-  ListBucketsCommand,
-  ListObjectsV2Command,
-} from "@aws-sdk/client-s3";
+import type { User } from "@prisma/client";
 
+import { S3Client, PutObjectCommand, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+import { prisma } from "~/db/prisma.server";
 import { requiredServerEnv } from "~/utils/helpers.server";
 
-const S3_DOMAIN = requiredServerEnv("S3_DOMAIN");
+export const S3_DOMAIN = requiredServerEnv("S3_DOMAIN");
 const S3_BUCKET = requiredServerEnv("S3_BUCKET");
 const S3_ENDPOINT = requiredServerEnv("S3_ENDPOINT");
 const S3_REGION = requiredServerEnv("S3_REGION");
@@ -38,24 +36,6 @@ export const getBucket = async () => {
   return await s3.send(new ListBucketsCommand({ Bucket: S3_BUCKET }));
 };
 
-export const getUserObjects = async (userId: string) => {
-  const output = await s3.send(
-    new ListObjectsV2Command({
-      Bucket: S3_BUCKET,
-      Prefix: userPrefix(userId),
-    }),
-  );
-
-  const files = output.Contents?.map((o) => ({
-    url: S3_DOMAIN + "/" + o.Key,
-    key: o.Key,
-    size: o.Size,
-    lastModified: o.LastModified,
-  }));
-
-  return files ?? [];
-};
-
 export const uploadToBucket = async (data: Buffer, key: string) => {
   const publicUrl = S3_DOMAIN + "/" + key;
   const output = await s3.send(
@@ -69,9 +49,21 @@ export const uploadToBucket = async (data: Buffer, key: string) => {
   return [publicUrl, output] as const;
 };
 
-export const userUploadToBucket = async (userId: string, data: Buffer, filename: string) => {
-  const publicUrl = S3_DOMAIN + "/" + filename;
-  const key = userPrefix(userId) + "/" + filename;
+interface UserUploadOptions {
+  userId: User["id"];
+  fileId: string;
+  filename: string;
+  tags?: string[];
+}
+
+export const userUploadToBucket = async (data: Buffer, opts: UserUploadOptions) => {
+  const publicUrl = S3_DOMAIN + "/" + opts.filename;
+  const key = userPrefix(opts.userId) + "/" + opts.filename;
+
+  const tags =
+    opts.tags?.map((value) => ({
+      value,
+    })) ?? [];
 
   const output = await s3.send(
     new PutObjectCommand({
@@ -80,6 +72,20 @@ export const userUploadToBucket = async (userId: string, data: Buffer, filename:
       Body: data,
     }),
   );
+
+  await prisma.file.create({
+    data: {
+      id: opts.fileId,
+      s3Key: key,
+      uploaderId: opts.userId,
+      size: Buffer.byteLength(data),
+      tag: {
+        createMany: {
+          data: tags,
+        },
+      },
+    },
+  });
 
   return [publicUrl, output] as const;
 };
