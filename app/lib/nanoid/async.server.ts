dd0002214c7a5947ec9c4c2bab3a3fe0045e4180
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-11-12 21:35:41 +0400

    feat(lib): re-exported `nanoid` with default config

---
+++ app/lib/nanoid/async.server.ts
@@ -0,0 +1,6 @@
+import { customAlphabet } from "nanoid/async";
+
+const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const nanoid = customAlphabet(alphabet, 21);
+
+export { nanoid };
