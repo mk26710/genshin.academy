8498ee3dca81ae8b9ceb81fa4924635495fb5794
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-11-14 12:33:08 +0400

    feat(responses): added `TypedJsonError` type which could be used later

--- app/utils/responses.server.ts
+++ app/utils/responses.server.ts
@@ -2,6 +2,13 @@ import type { ThrownResponse } from "@remix-run/react";
 
 import { json } from "@remix-run/node";
 
+export type TypedJsonError<Code = string, Message = string, Cause = Error> = {
+  code: Code | null;
+  message: Message | null;
+  details: Message | null;
+  cause: Cause | null;
+};
+
 export type TypedErrorResponse<Code = string, Message = string, Cause = Error> = {
   code?: Code;
   message?: Message;
