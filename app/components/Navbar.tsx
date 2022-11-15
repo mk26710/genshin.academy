ecfdf047fe7a7954745d174f0895f551b0fc54cb
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-11-15 10:49:29 +0400

    feat(components): updated `Navbar` with fresh styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,9 @@
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Menu, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars2Icon } from "@heroicons/react/24/solid";
 import { Form, NavLink } from "@remix-run/react";
 import clsx from "clsx";
 import { Fragment } from "react";
-import { generatePath } from "react-router";
 import { useTranslations } from "use-intl";
 
 import { UserAvatar } from "~/components/UserAvatar";
@@ -57,124 +57,103 @@ export function Navbar() {
   const visitorLocale = useVisitorLocale();
 
   return (
-    <Disclosure as="nav" className="sticky top-0 z-50 bg-neutral-800">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-[var(--max-content-width)] px-[var(--default-gap)] ">
-            <div className="relative flex h-[var(--header-height)] items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-neutral-400 hover:bg-neutral-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
+    <nav className="sticky top-0 z-50 h-[var(--header-height)] border-b border-gray-300 bg-white text-sm">
+      <div className="relative mx-auto flex h-full w-full max-w-[var(--max-content-width)] flex-row">
+        <div className="flex h-full flex-1  lg:hidden">
+          <Menu>
+            {({ open }) => (
+              <Fragment>
+                <Menu.Button className="px-4">
+                  {open ? <XMarkIcon className="h-6 w-6" /> : <Bars2Icon className="h-6 w-6" />}
+                </Menu.Button>
 
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">{/* there should be a log */}</div>
-                <div className="hidden sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map(({ i18nKey, href }) => (
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items
+                    as="div"
+                    className="fixed top-[calc(var(--header-height)-1px)] left-0 z-40 flex h-fit w-full flex-col space-y-1 border-b border-gray-300 bg-white px-2 pt-2 pb-3 text-base"
+                  >
+                    {navigation.map(({ href, i18nKey }) => (
                       <NavLink
                         key={i18nKey}
-                        to={generatePath(href, { visitorLocale })}
+                        to={href.replaceAll(":visitorLocale", visitorLocale)}
                         className={({ isActive }) =>
                           clsx(
-                            isActive
-                              ? "bg-neutral-900 text-white"
-                              : "text-neutral-300 hover:bg-neutral-700 hover:text-white",
-                            "rounded-md px-3 py-2 text-sm font-medium",
+                            "rounded-md px-3 py-2 font-medium text-gray-700",
+                            isActive && "bg-gray-200 text-black ",
                           )
                         }
                       >
                         {t(i18nKey, { count: 99 })}
                       </NavLink>
                     ))}
-                  </div>
-                </div>
-              </div>
+                  </Menu.Items>
+                </Transition>
+              </Fragment>
+            )}
+          </Menu>
+        </div>
 
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
-                {maybeUser != null && (
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="flex rounded-full bg-neutral-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-800">
-                        <span className="sr-only">Open user menu</span>
-                        <UserAvatar
-                          avatarUrl={maybeUser.avatarUrl}
-                          className="h-8 w-8 rounded-full bg-neutral-700"
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {userNavigation.map(({ i18nKey, href }) => (
-                          <Menu.Item key={i18nKey}>
-                            <NavLink
-                              to={href}
-                              className={({ isActive }) =>
-                                clsx(
-                                  isActive ? "bg-neutral-100" : "",
-                                  "block px-4 py-2 text-sm text-neutral-700",
-                                )
-                              }
-                            >
-                              {t(i18nKey)}
-                            </NavLink>
-                          </Menu.Item>
-                        ))}
+        <ul className="hidden h-full flex-1 flex-row gap-2 px-4 lg:flex">
+          {navigation.map(({ href, i18nKey }) => (
+            <li key={i18nKey} className="flex h-full">
+              <NavLink
+                to={href.replaceAll(":visitorLocale", visitorLocale)}
+                className={({ isActive }) =>
+                  clsx(
+                    "self-center rounded-md py-1.5 px-3.5  font-semibold",
+                    isActive ? "bg-neutral-100 text-black" : "text-neutral-600",
+                  )
+                }
+              >
+                {t(i18nKey, { count: 99 })}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
 
-                        <Menu.Item>
-                          <Form action="/logout" method="post">
-                            <button className="block px-4 py-2 text-sm text-neutral-700">
-                              {t("common.logout")}
-                            </button>
-                          </Form>
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                )}
-              </div>
-            </div>
-          </div>
+        {maybeUser != null && (
+          <Menu as="div" className="relative flex items-center pr-[var(--default-gap)]">
+            <Menu.Button>
+              <UserAvatar
+                avatarUrl={maybeUser.avatarUrl}
+                className="aspect-square h-8 w-8 border border-gray-300 bg-gray-100 object-cover"
+              />
+            </Menu.Button>
 
-          <Disclosure.Panel className="fixed top-[var(--header-height)] left-0 w-full bg-neutral-800 sm:hidden ">
-            <div className="space-y-1 px-2 pt-2 pb-3">
-              {navigation.map(({ i18nKey, href }) => (
-                <Disclosure.Button key={i18nKey} as={Fragment}>
-                  <NavLink
-                    to={generatePath(href, { visitorLocale })}
-                    className={({ isActive }) =>
-                      clsx(
-                        isActive
-                          ? "bg-neutral-900 text-white"
-                          : "text-neutral-300 hover:bg-neutral-700 hover:text-white",
-                        "block rounded-md px-3 py-2 text-base font-medium",
-                      )
-                    }
-                  >
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 top-0 z-50 mt-[calc(var(--header-height)_+_0.5rem)] flex min-w-[200px] flex-col rounded-md border border-gray-300 bg-white py-2 text-sm font-medium text-gray-700 shadow-lg">
+                {userNavigation.map(({ href, i18nKey }) => (
+                  <Menu.Item key={i18nKey} as={NavLink} to={href} className="px-4 py-2">
                     {t(i18nKey, { count: 99 })}
-                  </NavLink>
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+                  </Menu.Item>
+                ))}
+
+                <Menu.Item as={Form} action="/logout" method="post" className="px-4 py-2">
+                  <button type="submit" className="flex h-full w-full items-center">
+                    {t("common.logout")}
+                  </button>
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        )}
+      </div>
+    </nav>
   );
 }
