9aecd6d9f0d1d935e7c2e6cfdce0e46bf454c374
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-12-14 13:20:36 +0400

    feat(validators): updated validaiton schemas for user

--- app/schemas/user.server.ts
+++ app/schemas/user.server.ts
@@ -2,28 +2,30 @@ import { PermissionFlag, UserRole } from "@prisma/client";
 
 import { z } from "~/lib/zod.server";
 
-export const UserName = z
+export const UserNameSchema = z
   .string()
   .trim()
-  .min(3, "Username is too short.")
-  .max(50, "Why would you username be so long?");
+  .min(3, "Username must contain at least 3 characters.")
+  .max(50, "Username must not exceed 50 characters.")
+  .regex(/^[a-z_\d]$/, "Username must contain only lowercase latin characters, digits and _.")
+  .transform((s) => s.toLowerCase());
 
-export const UserPassword = z
+export const PasswordSchema = z
   .string()
   .trim()
   .min(8, "Password must contain at least 8 characters.")
-  .max(128, "Password is too long, do not exceed 128 characters.");
+  .max(128, "Password must not exceed 128 characters.");
 
 export const UserNameAndPassword = z.object({
-  name: UserName,
-  password: UserPassword,
+  name: UserNameSchema,
+  password: PasswordSchema,
 });
 
 export const PasswordNewPasswordRepeatNew = z
   .object({
-    currentPassword: UserPassword,
-    newPassword: UserPassword,
-    newPasswordRepeat: UserPassword,
+    currentPassword: PasswordSchema,
+    newPassword: PasswordSchema,
+    newPasswordRepeat: PasswordSchema,
   })
   .refine((obj) => obj.newPassword === obj.newPasswordRepeat, {
     message: "Repeated password doesn't match the new password.",
