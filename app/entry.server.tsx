dc9bfbbd63d781c9c98b54fea3e6448b352d2032
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2023-01-30 18:07:39 +0400

    feat(app): updated `entry.server.tsx`

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -15,15 +15,24 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const callbackName = isbot(request.headers.get("user-agent")) ? "onAllReady" : "onShellReady";
+  return isbot(request.headers.get("user-agent"))
+    ? handleBotRequest(request, responseStatusCode, responseHeaders, remixContext)
+    : handleBrowserRequest(request, responseStatusCode, responseHeaders, remixContext);
+}
 
+function handleBotRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
   return new Promise((resolve, reject) => {
     let didError = false;
 
     const { pipe, abort } = renderToPipeableStream(
       <RemixServer context={remixContext} url={request.url} />,
       {
-        [callbackName]: () => {
+        onAllReady() {
           const body = new PassThrough();
 
           responseHeaders.set("Content-Type", "text/html");
@@ -37,10 +46,51 @@ export default function handleRequest(
 
           pipe(body);
         },
-        onShellError: (err: unknown) => {
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          didError = true;
+
+          console.error(error);
+        },
+      },
+    );
+
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+
+function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
+  return new Promise((resolve, reject) => {
+    let didError = false;
+
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        onShellReady() {
+          const body = new PassThrough();
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            }),
+          );
+
+          pipe(body);
+        },
+        onShellError(err: unknown) {
           reject(err);
         },
-        onError: (error: unknown) => {
+        onError(error: unknown) {
           didError = true;
 
           console.error(error);
