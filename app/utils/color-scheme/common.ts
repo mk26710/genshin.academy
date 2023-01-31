08ceea317c7e94ca38de5c7239845396126faf4a
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2023-01-31 19:20:46 +0400

    style(utils): wrapped typeof with braces

--- app/utils/color-scheme/common.ts
+++ app/utils/color-scheme/common.ts
@@ -1,6 +1,6 @@
 export const SUPPORTED_COLOR_SCHEMES = ["light", "dark"] as const;
 
-export type ColorScheme = typeof SUPPORTED_COLOR_SCHEMES[number];
+export type ColorScheme = (typeof SUPPORTED_COLOR_SCHEMES)[number];
 
 export const isColorScheme = (value: unknown): value is ColorScheme => {
   if (SUPPORTED_COLOR_SCHEMES.findIndex((cs) => cs === value) === -1) {
