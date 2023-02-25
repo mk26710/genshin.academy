bab863fbe0003f45bd8ec2d44b7c3cb7c5b88468
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2023-02-25 17:23:39 +0400

    feat(components): cva for main is redundant now

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,101 +1,15 @@
 import type { VariantProps } from "class-variance-authority";
-import type { FC, HTMLAttributes } from "react";
+import type { ComponentPropsWithRef, FC, HTMLAttributes } from "react";
 
 import { cva } from "class-variance-authority";
+import clsx from "clsx";
 
 // Root <main>
+type MainRootProps = ComponentPropsWithRef<"main">;
 
-type MainRootVariantProps = VariantProps<typeof rootCva>;
-
-const rootCva = cva(null, {
-  variants: {
-    minH: {
-      availableScreen: "min-h-screen-available",
-      full: "min-h-full",
-    },
-    h: {
-      availableScreen: "h-screen-available",
-      auto: "h-auto",
-      full: "h-full",
-      fit: "h-fit",
-      max: "h-max",
-    },
-    maxW: {
-      screen: "max-w-[100vw]",
-      full: "max-w-full",
-      fit: "max-w-fit",
-      max: "max-w-max",
-      min: "max-w-min",
-    },
-    w: {
-      screen: "max-w-[100vw]",
-      full: "w-full",
-      auto: "w-auto",
-    },
-    display: {
-      flex: "flex",
-      block: "block",
-    },
-    direction: {
-      row: "flex-row",
-      col: "flex-col",
-    },
-    wrap: {
-      wrap: "flex-wrap",
-      reverse: "flex-wrap-reverse",
-      nowrap: "flex-nowrap",
-    },
-    p: {
-      0.5: "px-0.5 py-0.5",
-      1: "px-1 py-1",
-      2: "px-2 py-2",
-      3: "px-3 py-3",
-      4: "px-4 py-4",
-      5: "px-5 py-5",
-      6: "px-6 py-6",
-    },
-  },
-  defaultVariants: {
-    minH: "availableScreen",
-    h: "full",
-    maxW: "screen",
-    w: "full",
-    display: "flex",
-    direction: "col",
-    wrap: "wrap",
-    p: 4,
-  },
-});
-
-type MainRootProps = HTMLAttributes<HTMLDivElement> & MainRootVariantProps;
-
-const MainRoot: FC<MainRootProps> = ({
-  minH,
-  h,
-  maxW,
-  w,
-  display,
-  direction,
-  wrap,
-  p,
-  className,
-  children,
-  ...props
-}) => {
-  const cls = rootCva({
-    minH,
-    h,
-    maxW,
-    w,
-    display,
-    direction,
-    wrap,
-    p,
-    className,
-  });
-
+const MainRoot: FC<MainRootProps> = ({ className, children, ...props }) => {
   return (
-    <main className={cls} {...props}>
+    <main className={clsx("main", className)} {...props}>
       {children}
     </main>
   );
