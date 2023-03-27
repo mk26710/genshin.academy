553413ee8d089a36d3ed361ad687bf62c412b2b3
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2023-03-27 22:01:27 +0400

    feat(components): cahracter meta form now styled with daisy

--- app/components/forms/character-meta.tsx
+++ app/components/forms/character-meta.tsx
@@ -9,10 +9,6 @@ import {
 import { Form } from "@remix-run/react";
 import { useId } from "react";
 
-import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
-
 type SharedProps = {
   notEditableId?: boolean;
 };
@@ -37,198 +33,222 @@ export const CharacterMetaForm: FC<Props> = (props) => {
   const id = useId();
 
   return (
-    <Form
-      method="post"
-      className="flex flex-col gap-2 rounded-box bg-white p-4 py-5 shadow sm:p-6 "
-    >
-      <h2 className="font-semibold text-gray-900">
-        {props.editMode !== true && "New Character: Meta"}
-        {props.editMode === true && "Editing Character: Meta"}
-      </h2>
-      <p className="mb-4 text-sm text-gray-700">
-        {props.editMode !== true &&
-          "This form will create a new character meta entry and after that you'll be able to create public entry data in specific locales"}
-        {props.editMode === true && "This form lets you edit meta data of an existing character"}
-      </p>
+    <Form method="post" className="daisy-card bg-base-200">
+      <div className="daisy-card-body flex flex-col gap-2">
+        <h2 className="font-semibold">
+          {props.editMode !== true && "New Character: Meta"}
+          {props.editMode === true && "Editing Character: Meta"}
+        </h2>
+        <p className="mb-4 text-sm">
+          {props.editMode !== true &&
+            "This form will create a new character meta entry and after that you'll be able to create public entry data in specific locales"}
+          {props.editMode === true && "This form lets you edit meta data of an existing character"}
+        </p>
 
-      <div className="flex flex-col gap-1">
-        <Label htmlFor={id + "id"}>Character ID</Label>
-        <input
-          placeholder="e.g. kamisato-ayaka"
-          id={id + "id"}
-          name="id"
-          defaultValue={props.defaultValues?.id}
-          readOnly={props.notEditableId}
-          required
-          className="input select-none read-only:bg-gray-200 read-only:text-gray-700 read-only:hover:cursor-not-allowed"
-        />
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <Label htmlFor={id + "accentColor"}>Accent Color</Label>
-        <Input
-          placeholder="#ff0000"
-          id={id + "accentColor"}
-          name="accentColor"
-          defaultValue={props.defaultValues?.accentColor ?? undefined}
-          required
-        />
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <Label htmlFor={id + "rarity"}>Rarity</Label>
-        <Input
-          id={id + "rarity"}
-          name="rarity"
-          type="number"
-          min="4"
-          max="5"
-          step="1"
-          defaultValue={props.defaultValues?.rarity ?? "4"}
-          required
-        />
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <Label htmlFor={id + "element"}>Element</Label>
-        <select
-          className="select"
-          id={id + "element"}
-          name="element"
-          defaultValue={props.defaultValues?.element ?? -1}
-          required
-        >
-          <option disabled value={-1}>
-            -- select some genshin element --
-          </option>
-          {Object.values(GenshinElement).map((el) => (
-            <option key={el} value={el}>
-              {el}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <Label htmlFor={id + "weapon"}>Weapon</Label>
-        <select
-          className="select"
-          id={id + "weapon"}
-          name="weapon"
-          defaultValue={props.defaultValues?.weapon ?? -1}
-          required
-        >
-          <option disabled value={-1}>
-            -- select some genshin weapon --
-          </option>
-          {Object.values(GenshinWeapon).map((w) => (
-            <option key={w} value={w}>
-              {w}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <div className="flex flex-row items-center gap-2">
+        <div className="daisy-form-control">
+          <label htmlFor={id + "id"} className="daisy-label">
+            <span className="daisy-label-text">Character ID</span>
+          </label>
           <input
-            type="checkbox"
-            className="checkbox"
-            id={id + "hasVision"}
-            name="hasVision"
-            defaultChecked={props.defaultValues?.hasVision ?? undefined}
+            placeholder="e.g. kamisato-ayaka"
+            id={id + "id"}
+            name="id"
+            defaultValue={props.defaultValues?.id}
+            readOnly={props.notEditableId}
+            required
+            className="daisy-input-bordered daisy-input select-none"
           />
-          <Label htmlFor={id + "hasVision"} className="select-none">
-            Vision Owner
-          </Label>
         </div>
 
-        <div className="flex flex-row items-center gap-2">
+        <div className="daisy-form-control">
+          <label htmlFor={id + "accentColor"} className="daisy-label">
+            <span className="daisy-label-text">Accent Color</span>
+          </label>
           <input
-            type="checkbox"
-            className="checkbox"
-            id={id + "isArchon"}
-            name="isArchon"
-            defaultChecked={props.defaultValues?.isArchon ?? undefined}
+            placeholder="#ff0000"
+            id={id + "accentColor"}
+            name="accentColor"
+            defaultValue={props.defaultValues?.accentColor ?? undefined}
+            className="daisy-input-bordered daisy-input select-none"
+            required
           />
-          <Label htmlFor={id + "isArchon"} className="select-none">
-            Archon
-          </Label>
         </div>
-      </div>
 
-      <div className="flex flex-col gap-1">
-        <Label htmlFor={id + "association"}>Association</Label>
-        <select
-          className="select"
-          id={id + "association"}
-          name="association"
-          defaultValue={props.defaultValues?.association ?? -1}
-          required
-        >
-          <option disabled value={-1}>
-            -- select some genshin association --
-          </option>
-          {Object.values(GenshinAssociation).map((a) => (
-            <option key={a} value={a}>
-              {a}
+        <div className="daisy-form-control">
+          <label htmlFor={id + "rarity"} className="daisy-label">
+            <span className="daisy-label-text">Rarity</span>
+          </label>
+          <input
+            id={id + "rarity"}
+            name="rarity"
+            type="number"
+            min="4"
+            max="5"
+            step="1"
+            defaultValue={props.defaultValues?.rarity ?? "4"}
+            className="daisy-input-bordered daisy-input select-none"
+            required
+          />
+        </div>
+
+        <div className="daisy-form-control">
+          <label htmlFor={id + "element"} className="daisy-label">
+            <span className="daisy-label-text">Element</span>
+          </label>
+          <select
+            className="daisy-select-bordered daisy-select"
+            id={id + "element"}
+            name="element"
+            defaultValue={props.defaultValues?.element ?? -1}
+            required
+          >
+            <option disabled value={-1}>
+              -- select some genshin element --
             </option>
-          ))}
-        </select>
-      </div>
+            {Object.values(GenshinElement).map((el) => (
+              <option key={el} value={el}>
+                {el}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <div className="flex flex-col gap-x-2 gap-y-6 md:flex-row">
-        <div className="flex flex-col gap-1">
-          <Label htmlFor={id + "birthDay"}>Day of Birth</Label>
-          <Input
-            type="number"
-            min="1"
-            max="31"
-            step="1"
-            defaultValue={props.defaultValues?.birthDay ?? "31"}
-            id={id + "birthDay"}
-            name="birthDay"
+        <div className="daisy-form-control">
+          <label htmlFor={id + "weapon"} className="daisy-label">
+            <span className="daisy-label-text">Weapon</span>
+          </label>
+          <select
+            className="daisy-select-bordered daisy-select"
+            id={id + "weapon"}
+            name="weapon"
+            defaultValue={props.defaultValues?.weapon ?? -1}
+            required
+          >
+            <option disabled value={-1}>
+              -- select some genshin weapon --
+            </option>
+            {Object.values(GenshinWeapon).map((w) => (
+              <option key={w} value={w}>
+                {w}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex flex-col">
+          <div className="daisy-form-control flex-row items-center gap-2">
+            <input
+              type="checkbox"
+              className="daisy-checkbox-primary daisy-checkbox"
+              id={id + "hasVision"}
+              name="hasVision"
+              defaultChecked={props.defaultValues?.hasVision ?? undefined}
+            />
+            <label htmlFor={id + "hasVision"} className="daisy-label select-none">
+              <span className="daisy-label-text">Vision Owner</span>
+            </label>
+          </div>
+
+          <div className="daisy-form-control flex-row items-center gap-2">
+            <input
+              type="checkbox"
+              className="daisy-checkbox-primary daisy-checkbox"
+              id={id + "isArchon"}
+              name="isArchon"
+              defaultChecked={props.defaultValues?.isArchon ?? undefined}
+            />
+            <label htmlFor={id + "isArchon"} className="daisy-label select-none">
+              <span className="daisy-label-text">Archon</span>
+            </label>
+          </div>
+        </div>
+
+        <div className="daisy-form-control">
+          <label htmlFor={id + "association"} className="daisy-label ">
+            <span className="daisy-label-text">Association</span>
+          </label>
+          <select
+            className="daisy-select-bordered daisy-select"
+            id={id + "association"}
+            name="association"
+            defaultValue={props.defaultValues?.association ?? -1}
+            required
+          >
+            <option disabled value={-1}>
+              -- select some genshin association --
+            </option>
+            {Object.values(GenshinAssociation).map((a) => (
+              <option key={a} value={a}>
+                {a}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex flex-col gap-x-2 gap-y-6 md:flex-row">
+          <div className="daisy-form-control">
+            <label htmlFor={id + "birthDay"} className="daisy-label">
+              <span className="daisy-label-text">Day of Birth</span>
+            </label>
+            <input
+              type="number"
+              min="1"
+              max="31"
+              step="1"
+              defaultValue={props.defaultValues?.birthDay ?? "31"}
+              id={id + "birthDay"}
+              name="birthDay"
+              className="daisy-input-bordered daisy-input select-none"
+            />
+          </div>
+          <div className="daisy-form-control">
+            <label htmlFor={id + "birthMonth"} className="daisy-label">
+              <span className="daisy-label-text">Month of Birth</span>
+            </label>
+            <input
+              type="number"
+              min="1"
+              max="12"
+              step="1"
+              defaultValue={props.defaultValues?.birthMonth ?? "12"}
+              id={id + "birthMonth"}
+              name="birthMonth"
+              className="daisy-input-bordered daisy-input select-none"
+            />
+          </div>
+        </div>
+
+        <div className="daisy-form-control">
+          <label htmlFor={id + "releaseDate"} className="daisy-label">
+            <span className="daisy-label-text">Release Date</span>
+          </label>
+          <input
+            type="date"
+            id={id + "releaseDate"}
+            name="releaseDate"
+            required
+            defaultValue={props.defaultValues?.releaseDate ?? undefined}
+            className="daisy-input-bordered daisy-input w-fit"
           />
         </div>
-        <div className="flex flex-col gap-1">
-          <Label htmlFor={id + "birthMonth"}>Month of Birth</Label>
-          <Input
-            type="number"
-            min="1"
-            max="12"
-            step="1"
-            defaultValue={props.defaultValues?.birthMonth ?? "12"}
-            id={id + "birthMonth"}
-            name="birthMonth"
+
+        <div className="daisy-form-control">
+          <label htmlFor={id + "versionReleased"} className="daisy-label">
+            <span className="daisy-label-text">Release Release</span>
+          </label>
+          <input
+            id={id + "versionReleased"}
+            name="versionReleased"
+            placeholder="2.0"
+            defaultValue={props.defaultValues?.versionReleased}
+            className="daisy-input-bordered daisy-input"
           />
         </div>
-      </div>
 
-      <div className="flex flex-col gap-1">
-        <Label htmlFor={id + "releaseDate"}>Release Date</Label>
-        <Input
-          type="date"
-          id={id + "releaseDate"}
-          name="releaseDate"
-          className="w-fit"
-          required
-          defaultValue={props.defaultValues?.releaseDate ?? undefined}
-        />
+        <button type="submit" className="daisy-btn-primary daisy-btn mt-4">
+          Submit
+        </button>
       </div>
-
-      <div className="flex flex-col gap-1">
-        <Label htmlFor={id + "versionReleased"}>Release Release</Label>
-        <Input
-          id={id + "versionReleased"}
-          name="versionReleased"
-          placeholder="2.0"
-          defaultValue={props.defaultValues?.versionReleased}
-        />
-      </div>
-
-      <Button type="submit" variant="filled" color="primary">
-        Submit
-      </Button>
     </Form>
   );
 };
