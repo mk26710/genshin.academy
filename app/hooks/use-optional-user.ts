486ee39fdd4c3e5ff34832ef4837e7b95ff65495
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2023-04-09 21:24:30 +0400

    refactor(hooks): using new react router hook instead of matching

--- app/hooks/use-optional-user.ts
+++ app/hooks/use-optional-user.ts
@@ -1,16 +1,45 @@
 import type { GetUser } from "~/utils/session.server";
-
-import { useMatchesData } from "./use-matches-data";
+import { useRouteLoaderData } from "@remix-run/react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isUser(user: any): user is NonNullable<GetUser> {
-  return user && typeof user === "object" && typeof user.name === "string";
+  if (user == null || typeof user !== "object") {
+    return false;
+  }
+
+  if (!("id" in user)) {
+    return false;
+  }
+
+  if (!("name" in user)) {
+    return false;
+  }
+
+  if (typeof user.id !== "string") {
+    return false;
+  }
+
+  if (typeof user.name !== "string") {
+    return false;
+  }
+
+  return true;
 }
 
 export const useOptionalUser = (): GetUser | undefined => {
-  const data = useMatchesData("root");
-  if (!data || !isUser(data.user)) {
+  const data = useRouteLoaderData("root");
+
+  if (data == null || typeof data !== "object") {
     return undefined;
   }
+
+  if (!("user" in data)) {
+    return undefined;
+  }
+
+  if (!isUser(data.user)) {
+    return undefined;
+  }
+
   return data.user;
 };
