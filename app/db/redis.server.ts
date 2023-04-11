92204310e080f91829342f93928f5688cb108349
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2023-04-11 15:57:06 +0400

    feat(db): added redis factory

--- app/db/redis.server.ts
+++ app/db/redis.server.ts
@@ -1,6 +1,17 @@
-import Redis from "ioredis";
+import { Redis } from "ioredis";
 
 import { requiredServerEnv } from "~/utils/helpers.server";
+
+const createRedis = () => {
+  const REDIS_URL = requiredServerEnv("REDIS_URL");
+
+  let REDIS_PREFIX = requiredServerEnv("REDIS_PREFIX");
+  if (!REDIS_PREFIX.endsWith(":")) {
+    REDIS_PREFIX = REDIS_PREFIX + ":";
+  }
+
+  return new Redis(REDIS_URL, { keyPrefix: REDIS_PREFIX });
+};
 
 let redis: Redis;
 
@@ -9,18 +20,16 @@ declare global {
   var __redis__: Redis;
 }
 
-const REDIS_URL = requiredServerEnv("REDIS_URL");
-
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 // in production we'll have a single connection to the DB.
 if (process.env.NODE_ENV === "production") {
-  redis = new Redis(REDIS_URL);
+  redis = createRedis();
   console.log("[PROD] Redis client created");
 } else {
   if (!global.__redis__) {
-    global.__redis__ = new Redis(REDIS_URL);
+    global.__redis__ = createRedis();
     console.log("[DEV] Redis client created");
   }
   redis = global.__redis__;
