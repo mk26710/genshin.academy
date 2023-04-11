3c5732de96f97a5c3327a947c28327fe14fdb557
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2023-04-11 15:31:21 +0400

    feat(remix): enable v2 error boundary

--- remix.config.js
+++ remix.config.js
@@ -6,6 +6,7 @@ module.exports = {
     v2_routeConvention: true,
     v2_normalizeFormMethod: true,
     v2_meta: true,
+    v2_errorBoundary: true,
   },
   serverMinify: true,
   cacheDirectory: "./.cache/remix",
